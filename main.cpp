d4c7a101cf473a4ab34b973d25bbaaf88fa4457e
Edric Milaret <edric.ladent-milaret@savoirfairelinux.com>
2015-03-27 16:41:40 -0400

    Alpha-RC1 Version
    Change-Id: I69b44506dc52a18c47f102466472c68fbf08600b

---
+++ main.cpp
@@ -0,0 +1,38 @@
+/***************************************************************************
+ * Copyright (C) 2011-2015 by Savoir-Faire Linux                           *
+ * Author: Edric Ladent Milaret <edric.ladent-milaret@savoirfairelinux.com>*
+ *                                                                         *
+ * This program is free software; you can redistribute it and/or modify    *
+ * it under the terms of the GNU General Public License as published by    *
+ * the Free Software Foundation; either version 3 of the License, or       *
+ * (at your option) any later version.                                     *
+ *                                                                         *
+ * This program is distributed in the hope that it will be useful,         *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ * GNU General Public License for more details.                            *
+ *                                                                         *
+ * You should have received a copy of the GNU General Public License       *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
+ **************************************************************************/
+
+#include "mainwindow.h"
+#include <QApplication>
+#include <QFile>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+    QFile file(":/stylesheet.css");
+    if(file.open(QIODevice::ReadOnly | QIODevice::Text))
+    {
+        a.setStyleSheet(file.readAll());
+        file.close();
+    }
+
+    MainWindow w;
+    w.show();
+
+    return a.exec();
+}
---
+++ videowidget.cpp
@@ -0,0 +1,113 @@
+/***************************************************************************
+ * Copyright (C) 2011-2015 by Savoir-Faire Linux                           *
+ * Author: Edric Ladent Milaret <edric.ladent-milaret@savoirfairelinux.com>*
+ *                                                                         *
+ * This program is free software; you can redistribute it and/or modify    *
+ * it under the terms of the GNU General Public License as published by    *
+ * the Free Software Foundation; either version 3 of the License, or       *
+ * (at your option) any later version.                                     *
+ *                                                                         *
+ * This program is distributed in the hope that it will be useful,         *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ * GNU General Public License for more details.                            *
+ *                                                                         *
+ * You should have received a copy of the GNU General Public License       *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
+ **************************************************************************/
+
+#include "videowidget.h"
+
+#include <QDebug>
+
+VideoWidget::VideoWidget(QWidget *parent) :
+    QWidget(parent)
+  , previewRenderer_(nullptr)
+  , renderer_(nullptr)
+  , previewFrame_(nullptr)
+  , distantFrame_(nullptr)
+{
+    connect(Video::PreviewManager::instance(), SIGNAL(previewStarted(Video::Renderer*)), this, SLOT(previewStarted(Video::Renderer*)), Qt::ConnectionType::DirectConnection);
+    connect(Video::PreviewManager::instance(), SIGNAL(previewStopped(Video::Renderer*)), this, SLOT(previewStopped(Video::Renderer*)), Qt::ConnectionType::DirectConnection);
+    connect(CallModel::instance(), SIGNAL(rendererAdded(Call*,Video::Renderer*)), this, SLOT(callInitiated(Call*, Video::Renderer*)), Qt::ConnectionType::DirectConnection);
+}
+
+VideoWidget::~VideoWidget()
+{
+
+}
+
+void VideoWidget::previewStarted(Video::Renderer *renderer) {
+    if (previewRenderer_)
+        disconnect(previewRenderer_, 0,0,0);
+    previewRenderer_ = renderer;
+    connect(previewRenderer_, SIGNAL(frameUpdated()), this, SLOT(frameFromPreview()));
+    connect(previewRenderer_, SIGNAL(stopped()), this, SLOT(renderingStopped()), Qt::ConnectionType::DirectConnection);
+}
+
+void VideoWidget::previewStopped(Video::Renderer* renderer) {
+    Q_UNUSED(renderer)
+    disconnect(previewRenderer_, 0,0,0);
+    previewRenderer_ = nullptr;
+}
+
+void VideoWidget::frameFromPreview() {
+    if (previewFrame_) {
+        delete previewFrame_;
+        previewFrame_ = nullptr;
+    }
+    if (previewRenderer_ && previewRenderer_->isRendering()) {
+        const QSize size(previewRenderer_->size());
+        previewFrame_ = new QImage((const uchar*)previewRenderer_->currentFrame().constData(), size.width(), size.height(), QImage::Format_RGBA8888);
+    }
+    update();
+}
+
+void VideoWidget::paintEvent(QPaintEvent* evt) {
+    Q_UNUSED(evt)
+    QMutexLocker {&lock_};
+    QPainter painter(this);
+    //painter.drawRoundedRect(0,5,width()-5, height()-7,3,3);
+    if (distantFrame_ && renderer_ && renderer_->isRendering())
+          painter.drawImage(QRect(0,0,width(),height()),*(distantFrame_));
+    if (previewFrame_ && previewRenderer_ && previewRenderer_->isRendering()) {
+        int previewHeight = !renderer_ ? height() : height()/4;
+        int previewWidth = !renderer_  ? width() : width()/4;
+        int yPos = !renderer_ ? 0 : height() - previewHeight;
+        int xPos = !renderer_ ? 0 : width() - previewWidth;
+        painter.drawImage(QRect(xPos,yPos,previewWidth,previewHeight), *(previewFrame_));
+    }
+    painter.end();
+}
+
+void VideoWidget::callInitiated(Call* call, Video::Renderer *renderer) {
+    Q_UNUSED(call)
+    renderer_ = renderer;
+    connect(renderer_, SIGNAL(frameUpdated()), this, SLOT(frameFromDistant()));
+    connect(renderer_, SIGNAL(stopped()), this, SLOT(renderingStopped()), Qt::ConnectionType::DirectConnection);
+}
+
+void VideoWidget::frameFromDistant() {
+    QMutexLocker {&lock_};
+    if (distantFrame_) {
+        delete distantFrame_;
+        distantFrame_ = nullptr;
+    }
+    if (renderer_) {
+        const QSize size(renderer_->size());
+        distantFrame_ = new QImage((const uchar*) renderer_->currentFrame().constData(), size.width(), size.height(), QImage::Format_RGBA8888);
+    }
+    update();
+}
+
+void VideoWidget::renderingStopped() {
+    QMutexLocker {&lock_};
+    if (distantFrame_) {
+        delete distantFrame_;
+        distantFrame_ = nullptr;
+    }
+    disconnect(renderer_, 0,0,0);
+    disconnect(previewRenderer_, 0,0,0);
+    previewRenderer_ = nullptr;
+    renderer_ = nullptr;
+}
