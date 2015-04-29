c65553cac8420df770f3ba1179b2e625171558f9
Edric Milaret <edric.ladent-milaret@savoirfairelinux.com>
2015-04-29 16:33:38 -0400

    windows: add start on startup feature
    Refs #72072
    
    Change-Id: I2920f7f9d7dd8dfdd91f52a46ee33a106781fdbd

---
+++ utils.cpp
@@ -0,0 +1,81 @@
+/***************************************************************************
+ * Copyright (C) 2015 by Savoir-Faire Linux                           *
+ * Author: Edric Ladent Milaret <edric.ladent-milaret@savoirfairelinux.com>*
+ *                                                                         *
+ * This program is free software; you can redistribute it and/or modify    *
+ * it under the terms of the GNU General Public License as published by    *
+ * the Free Software Foundation; either version 3 of the License, or       *
+ * (at your option) any later version.                                     *
+ *                                                                         *
+ * This program is distributed in the hope that it will be useful,         *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ * GNU General Public License for more details.                            *
+ *                                                                         *
+ * You should have received a copy of the GNU General Public License       *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
+ **************************************************************************/
+
+#include "utils.h"
+
+bool
+Utils::CreateStartupLink() {
+    TCHAR userHome[MAX_PATH];
+    SHGetFolderPathW(NULL, CSIDL_PROFILE, NULL, 0, userHome);
+
+    TCHAR workingDirectory[MAX_PATH];
+    GetCurrentDirectory(MAX_PATH, workingDirectory);
+
+    std::wstring programPath(workingDirectory);
+    programPath += TEXT("\\RingClientWindows.exe");
+
+    std::wstring linkPath(userHome);
+    linkPath += TEXT("\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\Ring.lnk");
+
+    return Utils::CreateLink(programPath.c_str(), linkPath.c_str());
+}
+
+bool
+Utils::CreateLink(LPCWSTR lpszPathObj, LPCWSTR lpszPathLink) {
+    HRESULT hres;
+    IShellLink* psl;
+
+    hres = CoCreateInstance(CLSID_ShellLink, NULL,
+                            CLSCTX_INPROC_SERVER, IID_IShellLink, (LPVOID*)&psl);
+    if (SUCCEEDED(hres))
+    {
+        IPersistFile* ppf;
+        psl->SetPath(lpszPathObj);
+
+        hres = psl->QueryInterface(IID_IPersistFile, (LPVOID*)&ppf);
+        if (SUCCEEDED(hres))
+        {
+            hres = ppf->Save(lpszPathLink, TRUE);
+            ppf->Release();
+        }
+        psl->Release();
+    }
+    return hres;
+}
+
+void
+Utils::DeleteStartupLink() {
+    TCHAR userHome[MAX_PATH];
+    SHGetFolderPathW(NULL, CSIDL_PROFILE, NULL, 0, userHome);
+
+    std::wstring linkPath(userHome);
+    linkPath += TEXT("\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\Ring.lnk");
+
+    DeleteFile(linkPath.c_str());
+}
+
+bool
+Utils::CheckStartupLink() {
+    TCHAR userHome[MAX_PATH];
+    SHGetFolderPathW(NULL, CSIDL_PROFILE, NULL, 0, userHome);
+
+    std::wstring linkPath(userHome);
+    linkPath += TEXT("\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\Ring.lnk");
+    return PathFileExists(linkPath.c_str());
+}
+
--- utils.h
+++ utils.h
@@ -1,5 +1,5 @@
 /***************************************************************************
- * Copyright (C) 2011-2015 by Savoir-Faire Linux                           *
+ * Copyright (C) 2015 by Savoir-Faire Linux                           *
  * Author: Edric Ladent Milaret <edric.ladent-milaret@savoirfairelinux.com>*
  *                                                                         *
  * This program is free software; you can redistribute it and/or modify    *
@@ -16,53 +16,25 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
  **************************************************************************/
 
-#ifndef CONFIGURATIONWIDGET_H
-#define CONFIGURATIONWIDGET_H
+#ifndef UTILS_H
+#define UTILS_H
 
-#include <QWidget>
-#include <QItemSelection>
+#include <windows.h>
+#include <lmcons.h>
+#include <shobjidl.h>
+#include <shlguid.h>
+#include <shlobj.h>
+#include <shlwapi.h>
 
-#include "navwidget.h"
-#include "accountdetails.h"
+#include <string>
 
-#include "accountmodel.h"
-#include "video/devicemodel.h"
-#include "audio/codecmodel.h"
-
-namespace Ui {
-class ConfigurationWidget;
-}
-
-class ConfigurationWidget : public NavWidget
+class Utils
 {
-    Q_OBJECT
-
 public:
-    explicit ConfigurationWidget(QWidget *parent = 0);
-    ~ConfigurationWidget();
-    void atExit();
-
-//UI SLOTS
-private slots:
-    void on_deviceBox_currentIndexChanged(int index);
-    void on_sizeBox_currentIndexChanged(int index);
-    void on_rateBox_currentIndexChanged(int index);
-    void on_testVideoButton_toggled(bool checked);
-    void on_deleteAccountButton_clicked();
-    void on_addAccountButton_clicked();
-
-private slots:
-    void accountSelected(QItemSelection itemSel);
-
-    void on_startupBox_toggled(bool checked);
-
-private:
-    Ui::ConfigurationWidget *ui;
-    AccountModel* accountModel_;
-    Video::DeviceModel* deviceModel_;
-    CodecModel* codecModel_;
-    bool isLoading_;
-    AccountDetails* accountDetails_;
+    static bool CreateStartupLink();
+    static void DeleteStartupLink();
+    static bool CreateLink(LPCWSTR lpszPathObj, LPCWSTR lpszPathLink);
+    static bool CheckStartupLink();
 };
 
-#endif // CONFIGURATIONWIDGET_H
+#endif // UTILS_H
