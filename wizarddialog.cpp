005dae15b5c173a447d3c35085d4a26911bae026
Edric Milaret <edric.ladent-milaret@savoirfairelinux.com>
2015-04-29 17:02:31 -0400

    windows: add wizard at first startup
    Refs #71987
    
    Change-Id: I051c384c41b08af3fd65b83da25861d2fa52895d

---
+++ wizarddialog.cpp
@@ -0,0 +1,37 @@
+#include "wizarddialog.h"
+#include "ui_wizarddialog.h"
+
+#include "accountmodel.h"
+
+#include "utils.h"
+
+WizardDialog::WizardDialog(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::WizardDialog)
+{
+    ui->setupUi(this);
+
+    this->setWindowFlags(Qt::CustomizeWindowHint);
+    this->setWindowFlags(Qt::FramelessWindowHint);
+    this->setFixedSize(this->width(),this->height());
+    ui->buttonBox->setEnabled(false);
+}
+
+WizardDialog::~WizardDialog()
+{
+    delete ui;
+}
+
+void
+WizardDialog::on_buttonBox_accepted()
+{
+    AccountModel::instance()->add(ui->usernameEdit->text(), Account::Protocol::RING);
+    AccountModel::instance()->save();
+    Utils::CreateStartupLink();
+}
+
+void
+WizardDialog::on_usernameEdit_textChanged(const QString &arg1)
+{
+     ui->buttonBox->setEnabled(!arg1.isEmpty());
+}
