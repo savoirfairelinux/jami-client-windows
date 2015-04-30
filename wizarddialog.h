e0ea7d31956409acc298fafd469fdfdaefc5d7af
Edric Milaret <edric.ladent-milaret@savoirfairelinux.com>
2015-04-30 17:17:37 -0400

    windows: add contact backend
    squash: bump LRC api change
    Fix minor bug
    
    Refs #72110
    
    Change-Id: Ibe03b36fc8d45592c9c9e8425b66e2862feabb71

--- wizarddialog.h
+++ wizarddialog.h
@@ -1,7 +1,26 @@
+/***************************************************************************
+ * Copyright (C) 2015 by Savoir-Faire Linux                                *
+ * Author: Edric Ladent Milaret <edric.ladent-milaret@savoirfairelinux.com>*
+ *                                                                         *
+ * This program is free software; you can redistribute it and/or modify    *
+ * it under the terms of the GNU General Public License as published by    *
+ * the Free Software Foundation; either version 3 of the License, or       *
+ * (at your option) any later version.                                     *
+ *                                                                         *
+ * This program is distributed in the hope that it will be useful,         *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ * GNU General Public License for more details.                            *
+ *                                                                         *
+ * You should have received a copy of the GNU General Public License       *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
+ **************************************************************************/
+
 #ifndef WIZARDDIALOG_H
 #define WIZARDDIALOG_H
 
 #include <QDialog>
+#include <QtConcurrent/QtConcurrent>
 
 namespace Ui {
 class WizardDialog;
@@ -15,13 +34,16 @@ public:
     explicit WizardDialog(QWidget *parent = 0);
     ~WizardDialog();
 
+
 private slots:
-    void on_buttonBox_accepted();
-
     void on_usernameEdit_textChanged(const QString &arg1);
+    void accept();
 
+    void endSetup();
 private:
     Ui::WizardDialog *ui;
+private:
+    void setup();
 };
 
 #endif // WIZARDDIALOG_H
