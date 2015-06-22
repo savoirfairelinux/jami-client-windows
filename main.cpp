6c1f343f068635085f9f53ad14ab75768858d101
Edric Milaret <edric.ladent-milaret@savoirfairelinux.com>
2015-06-22 10:30:08 -0400

    debug: add an option to show debug console
    - works with -d or --debug
    
    Refs #76093
    
    Change-Id: Icac9ad4a140510bba13c003beda3e3194136d44e

--- main.cpp
+++ main.cpp
@@ -33,10 +33,34 @@
 
 REGISTER_MEDIA();
 
+void
+Console()
+{
+    AllocConsole();
+    freopen("CONOUT$", "w", stdout);
+    freopen("CONOUT$", "w", stderr);
+
+    COORD coordInfo;
+    coordInfo.X = 130;
+    coordInfo.Y = 9000;
+
+    SetConsoleScreenBufferSize(GetStdHandle(STD_OUTPUT_HANDLE), coordInfo);
+    SetConsoleMode(GetStdHandle(STD_OUTPUT_HANDLE),ENABLE_QUICK_EDIT_MODE| ENABLE_EXTENDED_FLAGS);
+}
+
 int
 main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    auto startMinimized = false;
+
+    for (auto string : QCoreApplication::arguments()) {
+        if (string == "-m" || string == "--minimized")
+            startMinimized = true;
+        if (string == "-d" || string == "--debug")
+            Console();
+    }
 
     QFont font;
     font.setFamily("Segoe UI");
@@ -50,13 +74,6 @@ main(int argc, char *argv[])
     }
 
     MainWindow w;
-
-    auto startMinimized = false;
-
-    for (auto string : QCoreApplication::arguments()) {
-        if (string == "-m" || string == "--minimized")
-            startMinimized = true;
-    }
 
     if (not startMinimized)
         w.show();
