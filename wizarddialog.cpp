f1072e88c52b27f5a7412d5e1a26c2c3205479df
Edric Milaret <edric.ladent-milaret@savoirfairelinux.com>
2015-07-09 16:53:03 -0400

    ring: set displayName for Ring account
    Refs #77362
    
    Change-Id: I87497043225d1c4f9fde3c6afe92ddc24bc19c5e

--- wizarddialog.cpp
+++ wizarddialog.cpp
@@ -67,6 +67,7 @@ WizardDialog::accept()
     Utils::CreateStartupLink();
 
     auto account = AccountModel::instance()->add(ui->usernameEdit->text(), Account::Protocol::RING);
+    account->setDisplayName(ui->usernameEdit->text());
     AccountModel::instance()->ip2ip()->setRingtonePath(Utils::GetRingtonePath());
     account->setRingtonePath(Utils::GetRingtonePath());
     account->setUpnpEnabled(true);
