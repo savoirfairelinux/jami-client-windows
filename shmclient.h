dbec6853111dc16ea485d0670cde04618000584f
Edric Milaret <edric.ladent-milaret@savoirfairelinux.com>
2015-08-04 13:41:03 -0400

    uri: register ring protocol
    - This commit enforce that only one instance of Ring can run.
    Ring window now show if it was minimized or hidden when attempting
    to launch again.
    
    - Ring will be launched or shown on ring: protocol use and
    call the provided uri.
    (e.g.
    <a href="ring:93fcda525734afd73f5cac945af0653906f6a692">
    	test ring link
    </a>)
    
    - Register ring protocol in Windows registry at install time
    as we need administrator elevation.
    
    Issue: #78588
    Issue: #78589
    Change-Id: I7645d8a3154ce1c91a91bfac21c330add9a03fcc

--- shmclient.h
+++ shmclient.h
@@ -1,5 +1,5 @@
 /***************************************************************************
- * Copyright (C) 2015-2016 by Savoir-faire Linux                                *
+ * Copyright (C) 2015 by Savoir-faire Linux                                *
  * Author: Edric Ladent Milaret <edric.ladent-milaret@savoirfairelinux.com>*
  *                                                                         *
  * This program is free software; you can redistribute it and/or modify    *
@@ -16,47 +16,26 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
  **************************************************************************/
 
-#pragma once
+#ifndef SHMCLIENT_H
+#define SHMCLIENT_H
 
-#include "globalsystemtray.h"
+#include <QThread>
+#include <QSystemSemaphore>
+#include <QSharedMemory>
 
-#include <QMainWindow>
-#include <QMouseEvent>
-
-#include "navstack.h"
-
-static constexpr char IDM_ABOUTBOX = 0x0010;
-
-class WindowBarUpOne;
-class MainWindowToolBar;
-
-namespace Ui {
-class MainWindow;
-}
-
-class MainWindow : public QMainWindow
+class ShmClient : public QThread
 {
     Q_OBJECT
-
 public:
-    explicit MainWindow(QWidget *parent = 0);
-    ~MainWindow();
-    void createThumbBar();
-
-protected:
-    bool nativeEvent(const QByteArray &eventType, void *message, long *result);
-
-public slots:
-    void onRingEvent(const QString& uri);
-
-private slots:
-    void trayActivated(QSystemTrayIcon::ActivationReason reason);
-    void onIncomingCall(Call *call);
-    void switchNormalMaximize();
+    ShmClient(QSharedMemory* shm, QSystemSemaphore* sem);
+    void run();
 
 private:
-    Ui::MainWindow* ui;
-    NavStack* navStack_;
-    WindowBarUpOne* wbOne_;
-    MainWindowToolBar* mwToolBar_;
+    QSharedMemory* shm_;
+    QSystemSemaphore* sem_;
+
+signals:
+    void RingEvent(const QString& uri = "");
 };
+
+#endif // SHMCLIENT_H
