d94e066044d40915f6473d4fba7b6845b1a35a2b
Eloi BAIL <eloi.bail@savoirfairelinux.com>
2015-09-21 13:58:31 -0400

    video: adapt to new LRC api
    Issue: #80643
    Change-Id: Iafaa844ce5cdef295b7818a6dd313afcf8e045a8

--- videowidget.h
+++ videowidget.h
@@ -47,8 +47,8 @@ public slots:
 private:
     Video::Renderer* previewRenderer_;
     Video::Renderer* renderer_;
-    std::shared_ptr<std::vector<unsigned char> > currentPreviewFrame_;
-    std::shared_ptr<std::vector<unsigned char> > currentDistantFrame_;
+    QImage* currentPreviewFrame_;
+    QImage* currentDistantFrame_;
     constexpr static int previewMargin_ = 15;
 };
 
