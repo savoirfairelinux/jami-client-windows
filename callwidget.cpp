9b9b544b401e5ba2cd23634a608a4b75f52cdfe2
Edric Milaret <edric.ladent-milaret@savoirfairelinux.com>
2016-01-06 10:51:18 -0500

    history: use sqlite history collection
    Change-Id: I022855a4fc0ccbfb1944d3981ccae23df9b4c5c8
    Tuleap: TBD

--- callwidget.cpp
+++ callwidget.cpp
@@ -33,7 +33,7 @@
 #include "person.h"
 #include "fallbackpersoncollection.h"
 #include "categorizedcontactmodel.h"
-#include "localhistorycollection.h"
+#include "sqlitehistorycollection.h"
 #include "media/text.h"
 #include "media/recording.h"
 #include "media/recordingmodel.h"
@@ -106,7 +106,7 @@ CallWidget::CallWidget(QWidget* parent) :
         ui->contactView->setItemDelegate(contactDelegate_);
 
         CategorizedHistoryModel::instance().
-                addCollection<LocalHistoryCollection>(LoadOptions::FORCE_ENABLED);
+                addCollection<SqliteHistoryCollection>(LoadOptions::FORCE_ENABLED);
 
         ui->historyList->setModel(CategorizedHistoryModel::SortedProxy::instance().model());
         CategorizedHistoryModel::SortedProxy::instance().model()->sort(0, Qt::DescendingOrder);
