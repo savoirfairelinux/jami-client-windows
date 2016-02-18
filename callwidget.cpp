2fc3709356d8de80dad0745788717f0759d9b4c7
Nicolas Jager <nicolas.jager@savoirfairelinux.com>
2016-02-18 13:26:12 -0500

    add new contact + open instant message windows
    - when pressing enter from ringContactLineEdit, the contact is added to
    the smartlist and the instant message window is opened.
    
    Change-Id: I9035c13926da45e7982eb29d1f47d62e816990cc
    Tuleap: #402

--- callwidget.cpp
+++ callwidget.cpp
@@ -580,6 +580,10 @@ CallWidget::on_ringContactLineEdit_returnPressed()
     ::time(&currentTime);
     cm->setLastUsed(currentTime);
     ui->ringContactLineEdit->clear();
+
+    QModelIndex index = RecentModel::instance().getIndex(cm);
+
+    RecentModel::instance().selectionModel()->setCurrentIndex(index, QItemSelectionModel::ClearAndSelect);
 }
 
 void
