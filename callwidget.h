442ddc507103406cef8217c8e1472b90740eb729
Edric Milaret <edric.ladent-milaret@savoirfairelinux.com>
2016-02-19 16:46:46 -0500

    ft: prototype file transfer ui
    Change-Id: Idb00ee3a07dde09b87a390459a435d71787b1b84
    Tuleap: #105

--- callwidget.h
+++ callwidget.h
@@ -82,6 +82,7 @@ private slots:
     void on_copyCMButton_clicked();
     void on_smartList_clicked(const QModelIndex &index);
     void on_shareButton_clicked();
+    void on_sendFileButton_clicked();
 
 private slots:
     void callIncoming(Call* call);
