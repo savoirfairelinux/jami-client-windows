a31bb3e238a256a4042ad864ee9a250f16bbb179
Nicolas Jager <nicolas.jager@savoirfairelinux.com>
2016-03-04 15:15:11 -0500

    fix: Video button appearing on many row
    Change-Id: I1de8f75a1c71faaa82799d173aeb2705b37839eb
    Tuleap: #441

--- smartlist.cpp
+++ smartlist.cpp
@@ -32,14 +32,18 @@ SmartList::SmartList(QWidget *parent) :
     verticalScrollBar()->hide();
 
     connect(this, &QAbstractItemView::entered, [this](const QModelIndex & index) {
-            removeCombar();
-            if (auto widget = indexWidget(index)) {
-                widget->setVisible(true);
-            } else {
+        auto widget = indexWidget(index);
+            if (!widget) {
                 ComBar* bar = new ComBar();
                 setIndexWidget(index, bar);
                 connect(bar, &ComBar::btnVideoClicked, this, [=](){ emit btnVideoClicked(); });
             }
+            else if (index.isValid())
+                indexWidget(index)->setVisible(true);
+
+            if(hoveredRow_.isValid() and indexWidget(hoveredRow_))
+                indexWidget(hoveredRow_)->setVisible(false);
+
             hoveredRow_ = index;
     });
 
@@ -63,9 +67,8 @@ SmartList::leaveEvent(QEvent* event)
 {
     Q_UNUSED(event);
 
+    hoveredRow_ = QModelIndex();
     verticalScrollBar()->hide();
-
-    removeCombar();
 }
 
 void
@@ -83,27 +86,22 @@ SmartList::eventFilter(QObject* watched, QEvent* event)
 {
 
     if (qobject_cast<QScrollBar*>(watched) && event->type() == QEvent::Enter) {
-        removeCombar();
+        hoveredRow_ = QModelIndex();
         return true;
     }
 
     return QObject::eventFilter(watched, event);
 }
 
-void
-SmartList::removeCombar()
-{
-    if(not hoveredRow_.isValid())
-        return;
-
-    if (auto widget = indexWidget(hoveredRow_)) {
-        widget->setVisible(false);
-    }
-}
 
 void
-SmartList::resizeEvent(QResizeEvent* event)
+SmartList::drawRow(QPainter* painter, const QStyleOptionViewItem& option, const QModelIndex& index) const
 {
-    reset();
-    QTreeView::resizeEvent(event);
+    if(index == hoveredRow_ && indexWidget(hoveredRow_))
+        indexWidget(index)->setVisible(true);
+        else if(indexWidget(index))
+            indexWidget(index)->setVisible(false);
+
+    QTreeView::drawRow(painter, option, index);
+
 }
