aa7ea5dbfbbccedd73e6e508d42af2fc397e3b72
Edric Milaret <edric.ladent-milaret@savoirfairelinux.com>
2016-03-08 11:00:48 -0500

    fix call state not being handled
    - Putting a call on hold then selecting another contact
    would render the person on hold unusable (no more reaction
    from UI on selection event)
    - Same applies to a failed call and having selected another
    contact
    
    Change-Id: I61239916d9d7778e8ce0ca557442b89433c91bbe
    Tuleap: #450

--- callwidget.cpp
+++ callwidget.cpp
@@ -456,6 +456,7 @@ void CallWidget::callStateToView(Call* value)
                 ui->stackedWidget->setCurrentWidget(ui->videoPage);
             break;
         case Call::State::CURRENT:
+        case Call::State::HOLD:
             ui->stackedWidget->setCurrentWidget(ui->videoPage);
             break;
         case Call::State::OVER:
@@ -464,6 +465,7 @@ void CallWidget::callStateToView(Call* value)
         case Call::State::INITIALIZATION:
         case Call::State::CONNECTED:
         case Call::State::RINGING:
+        case Call::State::FAILURE:
         case Call::State::ERROR:
             ui->stackedWidget->setCurrentWidget(ui->outboundCallPage);
             break;
