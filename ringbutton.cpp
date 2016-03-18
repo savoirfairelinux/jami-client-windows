b06563a5b54d601a5f1d659ae5423409e7c147ca
Nicolas Jager <nicolas.jager@savoirfairelinux.com>
2016-03-18 16:10:00 -0400

    fix/change buttons summoning file/folder chooser dialog
    - changes the generic QPushButtons used to open a file/folder chooser dialog
    for a new kind called RingButton.
    
    - icons are always at the right most position of the button.
    
    - removes some cssblob in stylesheet.
    
    Change-Id: Ie1c3832f6496e0e7de4bb7d120e7081483ec8785
    Tuleap: #482

---
+++ ringbutton.cpp
@@ -0,0 +1,47 @@
+/***************************************************************************
+ * Copyright (C) 2015-2016 by Savoir-faire Linux                           *
+ * Author: Jäger Nicolas <nicolas.jager@savoirfairelinux.com>              *
+ *                                                                         *
+ * This program is free software; you can redistribute it and/or modify    *
+ * it under the terms of the GNU General Public License as published by    *
+ * the Free Software Foundation; either version 3 of the License, or       *
+ * (at your option) any later version.                                     *
+ *                                                                         *
+ * This program is distributed in the hope that it will be useful,         *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ * GNU General Public License for more details.                            *
+ *                                                                         *
+ * You should have received a copy of the GNU General Public License       *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
+ **************************************************************************/
+
+#include "ui_ringbutton.h"
+
+#include "ringbutton.h"
+
+RingButton::RingButton(QWidget* parent) :
+    QWidget(parent),
+    ui(new Ui::RingButton)
+{
+    ui->setupUi(this);
+}
+
+RingButton::~RingButton()
+{
+    disconnect(this);
+    delete ui;
+}
+
+void
+RingButton::setText(const QString& text)
+{
+    ui->button->setText(text);
+    ui->button->resize(ui->button->sizeHint().width(), ui->button->sizeHint().height());
+}
+
+void
+RingButton::mousePressEvent(QMouseEvent *event)
+{
+    emit clicked();
+}
