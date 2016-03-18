01b45bd2d80eb83a07f56c9ac0030996138da07a
Nicolas Jager <nicolas.jager@savoirfairelinux.com>
2016-03-18 16:10:00 -0400

    fix/change buttons summoning file/folder chooser dialog
    - changes the generic QPushButtons used to open a file/folder chooser dialog
    for a new kind called RingButton.
    
    - icons are always at the right most position of the button.
    
    - removes some cssblob in stylesheet.
    
    Change-Id: Ie1c3832f6496e0e7de4bb7d120e7081483ec8785
    Tuleap: #482

---
+++ ringbutton.h
@@ -0,0 +1,43 @@
+/***************************************************************************
+ * Copyright (C) 2016 by Savoir-faire Linux                                *
+ * Author: Jäger Nicolas <nicolas.jager@savoirfairelinux.com>              *
+ *                                                                         *
+ * This program is free software; you can redistribute it and/or modify    *
+ * it under the terms of the GNU General Public License as published by    *
+ * the Free Software Foundation; either version 3 of the License, or       *
+ * (at your option) any later version.                                     *
+ *                                                                         *
+ * This program is distributed in the hope that it will be useful,         *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ * GNU General Public License for more details.                            *
+ *                                                                         *
+ * You should have received a copy of the GNU General Public License       *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
+ **************************************************************************/
+
+#pragma once
+
+#include <QWidget>
+
+namespace Ui {
+class RingButton;
+}
+
+class RingButton : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit RingButton(QWidget* parent = 0);
+    ~RingButton();
+    void setText(const QString& text);
+
+protected:
+    void mousePressEvent(QMouseEvent* event);
+
+private:
+    Ui::RingButton* ui;
+
+signals:
+    void clicked() const;
+};
