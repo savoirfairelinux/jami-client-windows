2e2470c88e731e941a6d74f6415a565f6f83d46f
Edric Milaret <edric.ladent-milaret@savoirfairelinux.com>
2016-04-18 16:10:44 -0400

    Rewrite instant messaging screen
    - HyperLink activated - YAY
    - No more bubble size problem
    - Username and date are now in an italic footer
    - Username and date display are now saved in user preferences
    - Display of delivery status using a lighter blue for message
    in SENDING status
    - Remove sender avatar from right message as it's kind of
    unnecessary
    
    * Need LRC patch connecting message engine stuff : 4059 *
    
    Change-Id: Id1adb1e96d99eb02a2c1e85a066dbbdc3e87cdc0
    Tuleap: #601
    Tuleap: #290

--- imdelegate.cpp
+++ imdelegate.cpp
@@ -19,36 +19,80 @@
 #include "imdelegate.h"
 
 #include <QApplication>
+#include <QTextDocument>
+#include <QSettings>
+#include <QDateTime>
 
 #include "media/text.h"
 #include "media/textrecording.h"
 
 #include "ringthemeutils.h"
+#include "settingskey.h"
 
 ImDelegate::ImDelegate(QObject *parent)
-    : QStyledItemDelegate(parent), showDate_(false), showAuthor_(false)
-{}
-
-void ImDelegate::setDisplayOptions(ImDelegate::DisplayOptions opt)
+    : QStyledItemDelegate(parent),
+      linkRegex_(
+          QRegularExpression(QStringLiteral("([a-z]{3,9}:|www\\.)([^\\s,.);!>]|[,.);!>](?!\\s|$))+"),
+                             QRegularExpression::CaseInsensitiveOption))
 {
-    showAuthor_ = opt & DisplayOptions::AUTHOR;
-    showDate_ = opt & DisplayOptions::DATE;
-    emit sizeHintChanged(QModelIndex());
+}
+
+QUrl ImDelegate::getParsedUrl(const QModelIndex& index)
+{
+    auto it = linkRegex_.globalMatch(index.data().toString());
+    while (it.hasNext()) {
+        QRegularExpressionMatch match = it.next();
+        return QUrl::fromUserInput(match.capturedRef().toString());
+    }
+    return QUrl();
 }
 
 void
 ImDelegate::formatMsg(const QModelIndex& index, QString& msg) const
 {
-    if (showAuthor_) {
-        auto author = index.data(
+    QSettings settings;
+    QStringList meta;
+    if (settings.value(SettingsKey::imShowAuthor).toBool()) {
+        meta << index.data(
                     static_cast<int>(Media::TextRecording::Role::AuthorDisplayname)).toString();
-        msg = QString("(%1)\n%2").arg(author, msg);
     }
-    if (showDate_) {
-        auto formattedDate = index.data(
-                    static_cast<int>(Media::TextRecording::Role::FormattedDate)).toString();
-        msg = QString("%2\n%1").arg(formattedDate, msg);
+    if (settings.value(SettingsKey::imShowDate).toBool()) {
+        auto timeStamp = index.data(
+                    static_cast<int>(Media::TextRecording::Role::Timestamp)).value<uint>();
+        auto date = QDateTime::fromTime_t(timeStamp);
+        auto now = QDateTime::currentDateTime();
+        if (now.date() == date.date())
+            meta << date.time().toString();
+        else
+            meta << date.toString();
     }
+    msg = QString("%2<footer><i>%1</i></footer>").arg(meta.join(" - "), msg);
+}
+
+QString
+ImDelegate::parseForLink(const QString& msg) const
+{
+    QString re;
+    auto p = 0;
+    auto it = linkRegex_.globalMatch(msg);
+    while (it.hasNext()) {
+        QRegularExpressionMatch match = it.next();
+        auto start = match.capturedStart();
+
+        auto url = QUrl::fromUserInput(match.capturedRef().toString());
+
+        if (start > p)
+            re.append(msg.mid(p, start - p).toHtmlEscaped().replace(QLatin1Char('\n'),
+                                                                    QStringLiteral("<br/>")));
+        re.append(QStringLiteral("<a href=\"%1\">%2</a>")
+                  .arg(QString::fromLatin1(url.toEncoded()).toHtmlEscaped(),
+                       match.capturedRef().toString().toHtmlEscaped()));
+        p = match.capturedEnd();
+    }
+    if (p < msg.size())
+        re.append(msg.mid(p, msg.size() - p));
+
+    return QString("<body>%1</body>").arg(re.isEmpty() ? msg : re);
 }
 
 void
@@ -67,7 +111,7 @@ ImDelegate::paint(QPainter* painter,
         auto msg = index.data(Qt::DisplayRole).toString();
         opt.text.clear();
         QStyle* style = opt.widget ? opt.widget->style() : QApplication::style();
-        QPalette::ColorGroup cg = opt.state & QStyle::State_Enabled
+        QPalette::ColorGroup cg = (opt.state & QStyle::State_Enabled)
                 ? QPalette::Normal : QPalette::Disabled;
         if (cg == QPalette::Normal && !(opt.state & QStyle::State_Active))
             cg = QPalette::Inactive;
@@ -76,57 +120,78 @@ ImDelegate::paint(QPainter* painter,
                 .value<Media::Media::Direction>() == Media::Media::Direction::IN
                 ? Qt::AlignLeft : Qt::AlignRight;
 
+        msg = parseForLink(msg);
         formatMsg(index, msg);
 
         QRect textRect = getBoundingRect(dir, msg, opt);
 
-        QRect bubbleRect(textRect.left() - padding_,
-                         textRect.top() - padding_,
-                         textRect.width() + 2 * padding_,
-                         textRect.height() + 2 * padding_ );
-
-        opt.decorationSize = iconSize_;
-        opt.decorationPosition = (dir == Qt::AlignRight ?
-                                      QStyleOptionViewItem::Right : QStyleOptionViewItem::Left);
-        opt.decorationAlignment = Qt::AlignTop | Qt::AlignHCenter;
+        if (dir == Qt::AlignLeft) {
+            opt.decorationSize = iconSize_;
+            opt.decorationPosition = (dir == Qt::AlignRight ?
+                                          QStyleOptionViewItem::Right : QStyleOptionViewItem::Left);
+            opt.decorationAlignment = Qt::AlignTop | Qt::AlignHCenter;
+        } else
+            opt.decorationSize = QSize();
         style->drawControl(QStyle::CE_ItemViewItem, &opt, painter, opt.widget);
 
         QPainterPath path;
-        path.addRoundedRect(bubbleRect, padding_, padding_);
+        path.addRoundedRect(textRect, padding_, padding_);
 
         if (dir == Qt::AlignRight) {
             painter->fillPath(path, RingTheme::blue_);
-            painter->setPen(Qt::white);
         }
         else {
             painter->fillPath(path, Qt::white);
-            painter->setPen(Qt::black);
         }
 
-        painter->drawText(textRect, Qt::AlignLeft | Qt::AlignTop | Qt::TextWordWrap, msg);
+        painter->save();
+
+        QTextDocument document;
+        document.setDefaultFont(fontMsg_);
+
+        if (dir == Qt::AlignRight)
+            document.setDefaultStyleSheet("body { color : white; } i { opacity: 100; font-size : 11px; text-align : right; }");
+        else
+            document.setDefaultStyleSheet("body { color : black; } i { opacity: 100; font-size : 11px; text-align : right; }");
+
+        document.setHtml(msg);
+
+        auto textOptions = QTextOption(Qt::AlignLeft);
+        textOptions.setWrapMode(QTextOption::WrapMode::WordWrap);
+        document.setDefaultTextOption(textOptions);
+        document.setTextWidth(textRect.width());
+
+        painter->translate(textRect.topLeft());
+        document.drawContents(painter);
+        painter->restore();
     }
 }
 
-QRect ImDelegate::getBoundingRect(const Qt::AlignmentFlag& dir, const QString& msg, const QStyleOptionViewItem &option) const
+QRect ImDelegate::getBoundingRect(const Qt::AlignmentFlag& dir,
+                                  const QString& msg,
+                                  const QStyleOptionViewItem &option) const
 {
-    QFont textFont = option.font;
-    QFontMetrics textFontMetrics(textFont);
     QRect textRect;
 
-    if (dir == Qt::AlignRight) {
-        textRect = textFontMetrics.boundingRect(option.rect.left() + 2 * padding_,
-                                                option.rect.top() + 2 * padding_,
-                                                option.rect.width() - iconSize_.width() - 4 * padding_,
-                                                0,
-                                                dir|Qt::AlignTop|Qt::TextWordWrap,
-                                                msg);
+    QTextDocument txtDoc;
+    txtDoc.setDefaultFont(fontMsg_);
+    txtDoc.setHtml(msg);
+    auto textOptions = QTextOption(Qt::AlignLeft);
+    textOptions.setWrapMode(QTextOption::WrapMode::WordWrap);
+    txtDoc.setDefaultTextOption(textOptions);
+
+    if (dir == Qt::AlignLeft) {
+        txtDoc.setTextWidth(option.rect.width() - iconSize_.width() - padding_);
+        textRect.setRect(option.rect.left() + iconSize_.width() + padding_,
+                         option.rect.top() + padding_,
+                         txtDoc.idealWidth(),
+                         txtDoc.size().height());
     } else {
-        textRect = textFontMetrics.boundingRect(option.rect.left() + iconSize_.width() + 2 * padding_,
-                                                option.rect.top() + 2 * padding_,
-                                                option.rect.width() - iconSize_.width() - 4 * padding_ ,
-                                                0,
-                                                dir|Qt::AlignTop|Qt::TextWordWrap,
-                                                msg);
+        txtDoc.setTextWidth(option.rect.width() - padding_);
+        textRect.setRect(option.rect.right() - padding_ - txtDoc.idealWidth(),
+                         option.rect.top() + padding_,
+                         txtDoc.idealWidth(),
+                         txtDoc.size().height());
     }
     return textRect;
 }
@@ -145,15 +210,18 @@ ImDelegate::sizeHint(const QStyleOptionViewItem& option,
             .value<Media::Media::Direction>() == Media::Media::Direction::IN
             ? Qt::AlignLeft : Qt::AlignRight;
 
+    msg = parseForLink(msg);
     formatMsg(index, msg);
 
     QRect boundingRect = getBoundingRect(dir, msg, opt);
 
-    QSize size(option.rect.width(), boundingRect.height() + padding_);
+    QSize size(option.rect.width(), boundingRect.height());
 
     /* Keep the minimum height needed. */
     if(size.height() < iconSize_.height())
-        size.setHeight(iconSize_.height() + padding_);
+        size.setHeight(iconSize_.height());
+
+    size.setHeight(size.height() + 2 * padding_);
 
     return size;
 }
