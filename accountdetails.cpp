a524d5f7364b5ad727b21c5d8ceb6f392e1c3a02
Anthony Léonard <anthony.leonard@savoirfairelinux.com>
2016-10-19 10:54:11 -0400

    ui: password for multi-device export cleared when leaving its page
    The password typed by the user to publish account on DHT was left in
    place when leaving the export page. This patch fix this undesirable
    behavior.
    
    Change-Id: I420573ec7e5af7cadd5bb112b566bbfbdb0a321c

--- accountdetails.cpp
+++ accountdetails.cpp
@@ -360,3 +360,10 @@ AccountDetails::on_cancelAddButton_clicked()
 {
     ui->devicesStackedWidget->setCurrentIndex(0);
 }
+
+void AccountDetails::on_devicesStackedWidget_currentChanged(int pageNum)
+{
+    // We clear the password textEdit each time we leave its page
+    if (pageNum != 1)
+        ui->passwordArchiveEdit->clear();
+}
