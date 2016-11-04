1f68bd34a908a71f999ffc0e98f83b8ed37735fd
Olivier SOLDANO <olivier.soldano@savoirfairelinux.com>
2016-11-04 15:38:29 -0400

    ui: Patch call state display
    when failed to call a peer, the next call showed
    "Failed" state before switching, now directly shows
    "searching for".
    
    when a call failed it was impossible to make another
    call after, because the precedent call wasn't refused
    patch fixes that
    
    Change-Id: I0d79b0d9482ef19890f20702c9fe305d3cef68a3

--- callwidget.cpp
+++ callwidget.cpp
@@ -419,7 +419,7 @@ CallWidget::callChangedSlot()
                     GlobalInstances::pixmapManipulator()
                     .callPhoto(actualCall_, QSize(130,130)).value<QImage>()));
 
-    if(actualCall_->state() == Call::State::OVER){
+    if(actualCall_->state() == Call::State::OVER || actualCall_->state() == Call::State::FAILURE){
         ui->outboundCallLabel->setText(actualCall_->toHumanStateName(Call::State::INITIALIZATION));
     } else {
         ui->outboundCallLabel->setText(actualCall_->toHumanStateName());
@@ -535,10 +535,11 @@ void CallWidget::callStateToView(Call* value)
         case Call::State::HOLD:
             ui->stackedWidget->setCurrentWidget(ui->videoPage);
             break;
+        case Call::State::OVER:
+            break;
         case Call::State::FAILURE:
         case Call::State::ERROR:
-        case Call::State::OVER:
-            ui->stackedWidget->setCurrentWidget(ui->welcomePage);
+            on_cancelButton_clicked();
             break;
         case Call::State::INITIALIZATION:
         case Call::State::CONNECTED:
@@ -886,4 +887,3 @@ CallWidget::contactLineEdit_registeredNameFound(const Account* account,NameDirec
         }
     }
 }
-
