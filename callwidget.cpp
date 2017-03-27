7b2a7e24d4d01707a2ca6c1c777825d8d5f3e2c1
Olivier SOLDANO <olivier.soldano@savoirfairelinux.com>
2017-03-27 16:06:53 -0400

    implements current account selection
    this patch implements the ui and logic of selecting the
    current account used in the ring-win32 client
    
    Change-Id: I60be8e352e15c7ffdd969f66df7e011f8212e40c

--- callwidget.cpp
+++ callwidget.cpp
@@ -142,6 +142,8 @@ CallWidget::CallWidget(QWidget* parent) :
         connect(&NameDirectory::instance(), SIGNAL(registeredNameFound(const Account*,NameDirectory::LookupStatus,const QString&,const QString&)),
                 this, SLOT(contactLineEdit_registeredNameFound(const Account*,NameDirectory::LookupStatus,const QString&,const QString&)));
 
+        ui->currentAccountWidget->update();
+
     } catch (const std::exception& e) {
         qDebug() << "INIT ERROR" << e.what();
     }
@@ -197,7 +199,8 @@ CallWidget::setupOutOfCallIM()
 }
 
 void
-CallWidget::onIncomingMessage(::Media::TextRecording* t, ContactMethod* cm) {
+CallWidget::onIncomingMessage(::Media::TextRecording* t, ContactMethod* cm)
+{
     Q_UNUSED(cm)
 
     if (!QApplication::focusWidget()) {
@@ -212,7 +215,8 @@ CallWidget::onIncomingMessage(::Media::TextRecording* t, ContactMethod* cm) {
 }
 
 void
-CallWidget::setupSmartListMenu() {
+CallWidget::setupSmartListMenu()
+{
     ui->smartList->setContextMenuPolicy(Qt::CustomContextMenu);
     connect(ui->smartList, &QListView::customContextMenuRequested, [=](const QPoint& pos){
         auto idx = ui->smartList->currentIndex();
@@ -435,7 +439,8 @@ CallWidget::atExit()
 {
 }
 
-void CallWidget::callStateToView(Call* value)
+void
+CallWidget::callStateToView(Call* value)
 {
     if (value) {
         switch (value->state()) {
@@ -495,6 +500,14 @@ CallWidget::on_cancelButton_clicked()
         actualCall_->performAction(Call::Action::REFUSE);
         ui->stackedWidget->setCurrentWidget(ui->welcomePage);
     }
+}
+
+void
+CallWidget::on_smartList_clicked(const QModelIndex& index)
+{
+    RecentModel::instance().selectionModel()->setCurrentIndex(
+                RecentModel::instance().peopleProxy()->mapToSource(index),
+                QItemSelectionModel::ClearAndSelect);
 }
 
 void
@@ -768,14 +781,6 @@ CallWidget::slidePage(QWidget* widget, bool toRight)
 }
 
 void
-CallWidget::on_smartList_clicked(const QModelIndex& index)
-{
-    RecentModel::instance().selectionModel()->setCurrentIndex(
-                RecentModel::instance().peopleProxy()->mapToSource(index),
-                QItemSelectionModel::ClearAndSelect);
-}
-
-void
 CallWidget::on_copyCMButton_clicked()
 {
     auto text = ui->contactMethodComboBox->currentText();
@@ -795,7 +800,7 @@ CallWidget::on_shareButton_clicked()
 }
 
 void
-CallWidget::contactLineEdit_registeredNameFound(const Account* account,NameDirectory::LookupStatus status,
+CallWidget::contactLineEdit_registeredNameFound(Account* account,NameDirectory::LookupStatus status,
                                                 const QString& address,const QString& name)
 {
     URI uri = URI(ui->ringContactLineEdit->text());
@@ -815,7 +820,7 @@ CallWidget::contactLineEdit_registeredNameFound(const Account* account,NameDirec
             qDebug() << uri;
             searchContactLineEditEntry(uri);
             auto cm = PhoneDirectoryModel::instance().getNumber(uri);
-            //cm->setAccount(account);
+            cm->setAccount(account);
             break;
         }
         case NameDirectory::LookupStatus::INVALID_NAME:
---
+++ currentaccountwidget.cpp
@@ -0,0 +1,108 @@
+#include "currentaccountwidget.h"
+#include "ui_currentaccountwidget.h"
+#include "globalinstances.h"
+#include "accountmodel.h"
+#include "account.h"
+#include "qstandarditemmodel.h"
+#include "profilemodel.h"
+#include "profile.h"
+#include "person.h"
+#include "utils.h"
+
+
+CurrentAccountWidget::CurrentAccountWidget(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::CurrentAccountWidget)
+{
+    ui->setupUi(this);
+    setup();
+}
+
+CurrentAccountWidget::~CurrentAccountWidget()
+{
+    delete ui;
+}
+
+void
+CurrentAccountWidget::setup()
+{
+    setupAccountSelector();
+
+}
+
+void
+CurrentAccountWidget::update()
+{
+    updateAccounts();
+
+}
+
+void
+CurrentAccountWidget::setupAccountSelector()
+{
+    ui->currentAccountSelector->setModel(&AccountModel::instance());
+    updateAccounts();
+}
+
+void
+CurrentAccountWidget::updateAccounts()
+{
+    auto selector = ui->currentAccountSelector;
+    AccountModel* model = &AccountModel::instance();
+    QModelIndex qIndex;
+    int firstEnabledIdx = -1;
+    for (int idx = model->rowCount() - 1; idx >= 0; idx--){
+        qIndex = selector->model()->index(idx, 0);
+        if (! model->getAccountByModelIndex(qIndex)->isEnabled()) {
+            qDebug() << model->getAccountByModelIndex(qIndex)->alias() << " Account is not enabled";
+            selector->setCurrentIndex(idx);
+            //qobject_cast<QStandardItemModel*>(selector->model())->item(idx)->setEnabled(false);
+        } else {
+            firstEnabledIdx = idx;
+        }
+    }
+
+    if (firstEnabledIdx >= 0) {
+        selector->setCurrentIndex(firstEnabledIdx);
+    }
+}
+
+
+
+void
+CurrentAccountWidget::setPhoto()
+{
+    auto selector = ui->currentAccountSelector;
+    if (selector->count() > 0) {
+        qDebug() << "currentAccountWidget height = " << height();
+        auto ac = static_cast<Account*>(selector->itemData(selector->currentIndex()).data());
+        if(&ProfileModel::instance()){
+            if (ProfileModel::instance().selectedProfile()) {
+                if (ProfileModel::instance().selectedProfile()->person()){
+                    QImage img = Utils::getCirclePhoto(ProfileModel::instance().selectedProfile()->person()->photo().value<QImage>(),
+                                                       ui->idDisplayLayout->contentsRect().height());
+                    ui->currentAccountPixmap->setPixmap(QPixmap::fromImage(img));
+                }
+            }
+        }
+    }
+}
+
+void
+CurrentAccountWidget::on_currentAccountSelector_currentIndexChanged(int index)
+{
+    QModelIndex idx = ui->currentAccountSelector->model()->index(index,0);
+    Account* ac = static_cast<AccountModel*>(ui->currentAccountSelector->model())->getAccountByModelIndex(idx);
+
+    if (ac) {
+        qDebug() << "current index changed triggered: account " << ac;
+        AccountModel::instance().setSelectedAccount(ac);
+        setPhoto();
+    } else {
+        qDebug() << "woooops no account @ idx " << index << ac;
+    }
+}
+
+void CurrentAccountWidget::on_accountDetailChanged(){
+    updateAccounts();
+}
