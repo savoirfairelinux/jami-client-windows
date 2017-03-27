dcd8ccf72b785844f8ddde6c167afd013aece122
Olivier SOLDANO <olivier.soldano@savoirfairelinux.com>
2017-03-27 16:00:06 -0400

    implements send contact request page
    This patch comprehends the ui of sending contact request and
    the logic behind it
    
    The ui is a new page on the stacked widget of callwidget
    it contains a text field displaying the RingID of the contact
    you want to request and another for a textual help,
    there is also a text entry for future text added to contact request
    payloads.
    
    Change-Id: I592f74fefa125d6e8cd576f9a49fbd74581cf4c9

---
+++ sendcontactrequestwidget.cpp
@@ -0,0 +1,46 @@
+#include <QDebug>
+
+#include "sendcontactrequestwidget.h"
+#include "ui_sendcontactrequestwidget.h"
+#include "account.h"
+#include "accountmodel.h"
+#include "recentmodel.h"
+#include "contactmethod.h"
+#include "phonedirectorymodel.h"
+
+SendContactRequestWidget::SendContactRequestWidget(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::SendContactRequestWidget)
+{
+    ui->setupUi(this);
+    ui->contactRequestInfo->setText("Contact Request Bruh!!!!\n");
+    ui->peerContactID->setText("ContactID\n");
+}
+
+SendContactRequestWidget::~SendContactRequestWidget()
+{
+    disconnect(sendCRClickedConnection_);
+    delete ui;
+}
+
+void
+SendContactRequestWidget::setup(const QModelIndex& nodeIdx)
+{
+    auto cmVector = RecentModel::instance().getContactMethods(nodeIdx);
+    disconnect(sendCRClickedConnection_);
+    QString number = cmVector[0]->uri();
+    ui->peerContactID->setText(number);
+    sendCRClickedConnection_ = connect(ui->sendContactRequestButton, &QPushButton::clicked, [number]() {
+        qDebug() << "number : " << number;
+        auto cm = PhoneDirectoryModel::instance().getNumber(number);
+        qDebug() << "cm ptr : " << cm;
+        qDebug() << "cm->account() ptr : " << cm->account();
+        if(cm->account() != nullptr){
+            cm->account()->sendContactRequest(cm);
+        } else {
+            qDebug() << "no account linked to contact method";
+            cm->setAccount(AccountModel::instance().selectedAccount());
+            cm->account()->sendContactRequest(cm);
+        }
+    });
+}
