60cec59b9a606dd58f0f38a758a85f025ab0af4f
Olivier SOLDANO <olivier.soldano@savoirfairelinux.com>
2017-03-27 16:06:53 -0400

    implements current account selection
    This patch implements the ui and logic of selecting the
    current account used in the ring-win32 client
    
    The selector is contained in a new dedicated widget
    which displays an image representing the account selected
    (for the moment there is no account related avatar so
    the picture is the one from the profile) and the alias of the account
    through a QComboBox
    
    Change-Id: I60be8e352e15c7ffdd969f66df7e011f8212e40c

---
+++ currentaccountwidget.h
@@ -0,0 +1,33 @@
+#ifndef CURRENTACCOUNTWIDGET_H
+#define CURRENTACCOUNTWIDGET_H
+
+#include <QWidget>
+
+namespace Ui {
+class CurrentAccountWidget;
+}
+
+class CurrentAccountWidget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit CurrentAccountWidget(QWidget *parent = 0);
+    ~CurrentAccountWidget();
+
+    void update();
+
+public slots:
+    void on_accountEnabledChanged();
+
+private slots:
+    void on_currentAccountSelector_currentIndexChanged(int index);
+
+private:
+    Ui::CurrentAccountWidget *ui;
+    void setup();
+    void setPhoto();
+    void updateAccounts();
+};
+
+#endif // CURRENTACCOUNTWIDGET_H
