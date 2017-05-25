5fa5419cf444f9a03ed01ec8d509953b13b9319e
Olivier SOLDANO <olivier.soldano@savoirfairelinux.com>
2017-05-25 11:43:40 -0400

    add framerate combobox in settings
    add a combobox for video framerate in settings,
    this patch only is not sufficient to provide framerate
    selection.
    
    Change-Id: I53b03be8610b064ccf80ef9e902e466522f98217

--- configurationwidget.h
+++ configurationwidget.h
@@ -75,6 +75,7 @@ private slots:
     void inputIndexChanged(int index);
     void accountPropertyChanged(Account* a, const QString& name,
                                 const QString& newVal, const QString& oldVal);
+    void framerateChanged(int idx);
 
 private:
     Ui::ConfigurationWidget *ui;
