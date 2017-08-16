7f6ca02bd170f2c5ffa31c477a0e8c16dbd893c7
Anthony Léonard <anthony.leonard@savoirfairelinux.com>
2017-08-16 14:27:31 -0400

    implement SmartList model for new LRC api
    A SmartListModel class is added as a wrapper of the new LRC model for
    conversations. It is meant to replace RecentModel use as the SmartList
    model and thus implements a QAbstractItemModel interface.
    
    The main method of this class is data() which returns every pieces of
    information we may want to display for any entry of the list (name,
    picture, number of unread messages, etc.)
    
    Change-Id: Idff90b6f547f35d9ee06e42a868760c329e6b1d1

---
+++ smartlistmodel.h
@@ -0,0 +1,66 @@
+/***************************************************************************
+ * Copyright (C) 2017 by Savoir-faire Linux                                *
+ * Author: Anthony Léonard <anthony.leonard@savoirfairelinux.com>          *
+ *                                                                         *
+ * This program is free software; you can redistribute it and/or modify    *
+ * it under the terms of the GNU General Public License as published by    *
+ * the Free Software Foundation; either version 3 of the License, or       *
+ * (at your option) any later version.                                     *
+ *                                                                         *
+ * This program is distributed in the hope that it will be useful,         *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ * GNU General Public License for more details.                            *
+ *                                                                         *
+ * You should have received a copy of the GNU General Public License       *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
+ **************************************************************************/
+
+#ifndef SMARTLISTMODEL_H
+#define SMARTLISTMODEL_H
+
+// Qt include
+#include <QAbstractItemModel>
+
+// LRC
+#include "api/account.h"
+#include "api/conversation.h"
+#include "api/contact.h"
+
+namespace lrc { namespace api { class ConversationModel; } }
+
+class SmartListModel : public QAbstractItemModel
+{
+    Q_OBJECT
+
+public:
+    using AccountInfo = lrc::api::account::Info;
+    using ConversationInfo = lrc::api::conversation::Info;
+    using ContactInfo = lrc::api::contact::Info;
+
+    enum Role {
+        DisplayName = Qt::UserRole+1,
+        DisplayID,
+        Picture,
+        Presence,
+        URI,
+        UnreadMessagesCount,
+        LastInteractionDate,
+        LastInteraction
+    };
+
+    explicit SmartListModel(AccountInfo& acc, QObject *parent = 0);
+
+    // QAbstractItemModel
+    int rowCount(const QModelIndex &parent = QModelIndex()) const override;
+    int columnCount(const QModelIndex &parent) const override;
+    QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
+    QModelIndex index(int row, int column, const QModelIndex &parent = QModelIndex()) const;
+    QModelIndex parent(const QModelIndex &child) const;
+    Qt::ItemFlags flags(const QModelIndex &index) const;
+
+private:
+    AccountInfo& acc_;
+};
+
+#endif // SMARTLISTMODEL_H
