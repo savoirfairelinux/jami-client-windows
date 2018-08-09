2de6a9302ec2560c9d5ce148f4bdaf99b4984303
Andreas Traczyk <andreas.traczyk@savoirfairelinux.com>
2018-08-09 15:41:52 -0400

    videocall: fix overlay not rendering at max opacity
    - fixes a known Qt bug present on macOS and windows that prevents
      rendering of widgets when a QGraphicsOpacityEffect with an
      opacity of 1.0 is attached.
    
    Change-Id: Ib287e9c48b66d699c427165f8c5ce7a55558b307

--- videoview.h
+++ videoview.h
@@ -23,6 +23,8 @@
 #include <QMouseEvent>
 #include <QPropertyAnimation>
 
+#include <limits.h>
+
 #include "videooverlay.h"
 
 namespace Ui {
@@ -68,9 +70,17 @@ private:
     bool draggingPreview_ = false;
     bool resizingPreview_ = false;
 
-    constexpr static int fadeOverlayTime_ = 2000; //msec
+    constexpr static int fadeOverlayTime_ = 1000; //msec
     constexpr static int resizeGrip_ = 40;
     constexpr static int minimalSize_ = 100;
+
+    // TODO: fix when changing Qt version
+    // Full(1.0) opacity bug affecting many Qt version (macOS + win10)
+    // causing the render to take a buggy code path which can be avoided
+    // by using opacity values other than precisely 1.0.
+    // https://bugreports.qt.io/browse/QTBUG-65981
+    // https://bugreports.qt.io/browse/QTBUG-66803
+    constexpr static qreal maxOverlayOpacity_ = 0.9999999999980000442;
 
 private:
     void toggleFullScreen();
