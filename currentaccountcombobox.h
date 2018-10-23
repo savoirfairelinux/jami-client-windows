f41a789b1f1d48e09e66ec30eefad4e92ee35d49
Isa Nanic <isa.nanic@savoirfairelinux.com>
2018-10-23 11:37:26 -0400

    current account combo box install in callWidget - untabified and revert avatar size
    Change-Id: I3ad675db56a27101d78ba13825f4618051084b53

---
+++ currentaccountcombobox.h
@@ -0,0 +1,46 @@
+/***************************************************************************
+ * Copyright (C) 2018 by Savoir-faire Linux                                *
+ * Author: Isa Nanic <isa.nanic@savoirfairelinux.com>                      *
+ * Author: Andreas Traczyk <andreas.traczyk@savoirfairelinux.com>          *
+ *                                                                         *
+ * This program is free software; you can redistribute it and/or modify    *
+ * it under the terms of the GNU General Public License as published by    *
+ * the Free Software Foundation; either version 3 of the License, or       *
+ * (at your option) any later version.                                     *
+ *                                                                         *
+ * This program is distributed in the hope that it will be useful,         *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ * GNU General Public License for more details.                            *
+ *                                                                         *
+ * You should have received a copy of the GNU General Public License       *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
+ **************************************************************************/
+#pragma once
+#include <QComboBox>
+#include "accountlistmodel.h"
+#include "accountitemdelegate.h"
+
+namespace Ui {
+    class CurrentAccountComboBox;
+}
+
+class CurrentAccountComboBox : public QComboBox
+{
+    Q_OBJECT
+        CurrentAccountComboBox(const CurrentAccountComboBox& cpy);
+
+public:
+    explicit CurrentAccountComboBox(QWidget* parent = nullptr);
+    ~CurrentAccountComboBox();
+
+private:
+    void importLabelPhoto(const int& index);
+    void paintEvent(QPaintEvent* e);
+
+    AccountItemDelegate* accountItemDelegate_;
+    std::unique_ptr<AccountListModel> accountListModel_;
+
+    QPixmap currentAccountAvatarImage_;
+    int cellHeight_ = 48; // [screen awareness]
+};
