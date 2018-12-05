b0236f8fd014c15a29f1ff784250d9feffd8dd6d
Andreas Traczyk <andreas.traczyk@savoirfairelinux.com>
2018-12-05 11:06:31 -0500

    build: add scripts to build client deps
    Change-Id: Ifa415c0a00edf89984720e6efc2dec1325bc46e7

--- fetch-deps.bat
+++ fetch-deps.bat
@@ -6,19 +6,25 @@ if "%1" == "/c" (
 	set cloneSubmodules=Y
 )
 
+set APPLY_CMD=git apply --reject --ignore-whitespace --whitespace=fix
+
 if not exist "winsparkle" (
 	git clone --depth=1 https://github.com/vslavik/winsparkle.git
-)
-
-if "%cloneSubmodules%" neq "N" (
-    cd winsparkle
-	git submodule init
-	git submodule update
+	cd winsparkle
+	git checkout 6e7bb8554852d7f964fd176c440dd29cc772f2dc
+	%APPLY_CMD% ..\winsparkle.patch
+	if "%cloneSubmodules%" neq "N" (
+		git submodule init
+		git submodule update
+	)
 	cd ..
 )
 
 if not exist "qrencode-win32" (
 	git clone --depth=1 https://github.com/BlueDragon747/qrencode-win32.git
+	cd qrencode-win32
+	git checkout d6495a2aa74d058d54ae0f1b9e9e545698de66ce
+	%APPLY_CMD% ..\qrencode-win32.patch
 )
 
 :cleanup
