72f6dc6f46ab0f0ada9d7e0c2ecb17062d6e8399
Sébastien Blin <sebastien.blin@savoirfairelinux.com>
2018-12-13 09:49:03 -0500

    chatview: add invitation top bar when necessary
    Change-Id: I70d3e10746ed8aa68b351e6d9e03516b82fab3b3
    Gitlab: #420

--- messagewebview.cpp
+++ messagewebview.cpp
@@ -2,7 +2,7 @@
  * Copyright (C) 2017-2018 by Savoir-faire Linux                           *
  * Author: Andreas Traczyk <andreas.traczyk@savoirfairelinux.com>          *
  * Author: Alexandre Viau <alexandre.viau@savoirfairelinux.com>            *
- * Author: Sébastien Blin <sebastien.blin@savoirfairelinux.com>            *
+ * Author: Sï¿½bastien Blin <sebastien.blin@savoirfairelinux.com>            *
  * Author: Hugo Lefeuvre <hugo.lefeuvre@savoirfairelinux.com>              *
  *                                                                         *
  * This program is free software; you can redistribute it and/or modify    *
@@ -213,10 +213,10 @@ MessageWebView::setSenderImage(const std::string& sender,
 }
 
 void
-MessageWebView::setInvitation(bool show, const std::string& contactUri)
+MessageWebView::setInvitation(bool show, const std::string& contactUri, const std::string& contactId)
 {
-    QString s = QString::fromLatin1(show ? "showInvitation(\"%1\")" : "showInvitation()")
-        .arg(QString(contactUri.c_str()));
+    QString s = QString::fromLatin1(show ? "showInvitation(\"%1\", \"%2\")" : "showInvitation()")
+        .arg(QString(contactUri.c_str())).arg(QString(contactId.c_str()));
     page()->runJavaScript(s, QWebEngineScript::MainWorld);
 }
 
--- messagewebview.h
+++ messagewebview.h
@@ -68,7 +68,7 @@ public:
                       lrc::api::interaction::Info> interactions);
     void setSenderImage(const std::string& sender,
                         const std::string& senderImage);
-    void setInvitation(bool show, const std::string& contactUri);
+    void setInvitation(bool show, const std::string& contactUri, const std::string& contactId);
     void hideMessages();
 
 private slots:
