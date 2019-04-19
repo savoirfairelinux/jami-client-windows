d6d42bc18e449336a34d42902ed607e07d4d9683
Andreas Traczyk <andreas.traczyk@savoirfairelinux.com>
2019-04-19 12:43:10 -0400

    cleanup: code quality and ui fixes to jami/SIP account settings
    Change-Id: I24ea360a70740138f3a7ac9e044079b3772d3365

--- settingswidget.h
+++ settingswidget.h
@@ -1,7 +1,7 @@
 /***************************************************************************
  * Copyright (C) 2019-2019 by Savoir-faire Linux                           *
- * Author: Isa Nanic <isa.nanic@savoirfairelinux.com>
- * Author: Mingrui Zhang <mingrui.zhang@savoirfairelinux.com>
+ * Author: Isa Nanic <isa.nanic@savoirfairelinux.com>                      *
+ * Author: Mingrui Zhang <mingrui.zhang@savoirfairelinux.com>              *
  *                                                                         *
  * This program is free software; you can redistribute it and/or modify    *
  * it under the terms of the GNU General Public License as published by    *
@@ -16,42 +16,34 @@
  * You should have received a copy of the GNU General Public License       *
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.   *
  **************************************************************************/
-
 #pragma once
-#include <QScrollArea>
-#include <QMovie>
 
-#include "navwidget.h"
+#include <QMovie>
+#include <QScrollArea>
+
 #include "lrcinstance.h"
+#include "navwidget.h"
 
 #include "advancedsettingswidget.h"
 #include "advancedsipsettingwidget.h"
 #include "bannedlistmodel.h"
-
 #include "linkdevwidget.h"
-#include "ui_linkdevwidget.h"
 
-// general settings
 #include "api/datatransfermodel.h"
 #include "typedefs.h"
-
-// av settings
 #include "video/devicemodel.h"
 
-namespace Ui
-{
+namespace Ui {
 class SettingsWidget;
 }
 
-class SettingsWidget : public NavWidget
-{
+class SettingsWidget : public NavWidget {
     Q_OBJECT
     SettingsWidget(const SettingsWidget& cpy);
 
 public:
     explicit SettingsWidget(QWidget* parent = nullptr);
     ~SettingsWidget();
-    void resize(int size);
 
     // NavWidget
     virtual void navigated(bool to);
@@ -59,59 +51,66 @@ public:
 public slots:
     virtual void slotAccountOnBoarded();
 
-public slots:
-    void updateSettings(int size);
-
 private:
-    enum Button {accountSettingsButton, generalSettingsButton, avSettingsButton};
-    enum RegName {BLANK, INVALIDFORM, TAKEN, FREE, SEARCHING};
-    enum List {DevList, BannedContacts};
+    Ui::SettingsWidget* ui;
+
+    enum Button {
+        accountSettingsButton,
+        generalSettingsButton,
+        avSettingsButton
+    };
+    enum RegName {
+        BLANK,
+        INVALIDFORM,
+        TAKEN,
+        FREE,
+        SEARCHING
+    };
+    enum List {
+        DevList,
+        BannedContacts
+    };
 
     void setSelected(Button sel);
     void updateAccountInfoDisplayed();
-
     void resizeEvent(QResizeEvent* event);
-
-    Ui::SettingsWidget* ui;
-
-    // *************************  SIP Account/Account Settings  *************************
-
-    lrc::api::account::ConfProperties_t confProps_;
-
     bool showOrHide_{false};
     void showhideButtonClicked();
     void passwordClicked();
     void avatarClicked();
-
     void afterNameLookup(lrc::api::account::LookupStatus status, const std::string& regName);
-    QString registeredName_;
-
     bool validateRegNameForm(const QString& regName);
+    void setRegNameUi(RegName stat);
+    void removeDeviceSlot(int index);
+    void unban(int index);
+    void setConnections();
+    void populateGeneralSettings();
+    void populateAVSettings();
+    void setFormatListForDevice(Video::Device* device);
+    void showPreview();
+    void startVideo();
+    void stopVideo();
+    void toggleVideoSettings(bool enabled);
+    void toggleVideoPreview(bool enabled);
 
+    QString registeredName_;
+    lrc::api::account::ConfProperties_t confProps_;
     AdvancedSettingsWidget* advancedSettingsWidget_;
     QScrollArea* scrollArea_;
     Button pastButton_ = Button::generalSettingsButton;
     bool advancedSettingsDropped_ = false;
     bool bannedContactsShown_ = false;
-
+    QList<QPair<int, int>> formatIndexList_;
+    Video::DeviceModel* deviceModel_;
+    QString currentDeviceName_;
     AdvancedSIPSettingsWidget* advancedSIPSettingsWidget_;
     QScrollArea* scrollSIPArea_;
     bool advancedSIPSettingsDropped_ = false;
-
     int avatarSize_;
     int avatarSIPSize_;
-
-    void setRegNameUi(RegName stat);
     bool regNameBtn_ = false;
-
     const int itemHeight_ = 55;
-
-    void removeDeviceSlot(int index);
-    void unban(int index);
-
-    void setConnections();
     QMovie* gif;
-
     LinkDevWidget* linkDevWidget;
 
 private slots:
@@ -123,25 +122,15 @@ private slots:
     void regNameRegisteredSlot();
     void setAccEnableSlot(int state);
     void delAccountSlot();
-
     void toggleAdvancedSIPSettings();
     void toggleAdvancedSettings();
     void toggleBannedContacts();
     void exportAccountSlot();
-
     void updateAndShowDevicesSlot();
     void updateAndShowBannedContactsSlot();
-
     void showLinkDevSlot();
     void showCurrentAccountSlot();
-
     void setButtonIconSlot(int frame);
-
-    // *************************  General Settings  *************************
-private:
-    void populateGeneralSettings();
-
-private slots:
     void setNotificationsSlot(int state);
     void checkForUpdateSlot();
     void setClosedOrMinSlot(int state);
@@ -150,27 +139,9 @@ private slots:
     void openRecordFolderSlot();
     void setUpdateIntervalSlot(int value);
     void setUpdateAutomaticSlot(int state);
-
-    // *************************  Audio/Visual Settings  *************************
-private:
-    QList<QPair<int, int>> formatIndexList_;
-    Video::DeviceModel* deviceModel_;
-    QString currentDeviceName_;
-
-    void populateAVSettings();
-    void setFormatListForDevice(Video::Device* device);
-    void showPreview();
-    void startVideo();
-    void stopVideo();
-    void toggleVideoSettings(bool enabled);
-    void toggleVideoPreview(bool enabled);
-
-private slots:
     void outputDevIndexChangedSlot(int index);
     void inputdevIndexChangedSlot(int index);
-
     void deviceModelIndexChanged(int index);
     void slotDeviceBoxCurrentIndexChanged(int index);
     void slotFormatBoxCurrentIndexChanged(int index);
-
 };
