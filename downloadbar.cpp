42e343baf3065f2095592dc7f4d8329ba45468c8
Ming Rui Zhang <mingrui.zhang@savoirfairelinux.com>
2019-04-30 16:30:24 -0400

    updatelogic: add qt download manger to handle Jami version verification, and new installer download. add new ui to handle user update event
    Change-Id: Id71232a8c32675391f9d46b0b5a7658e8a47fc95

---
+++ downloadbar.cpp
@@ -0,0 +1,52 @@
+/***************************************************************************
+ * Copyright (C) 2015-2019 by Savoir-faire Linux                           *
+ * Author: Mingrui Zhang <mingrui.zhang@savoirfairelinux.com>              *
+ *                                                                         *
+ * This program is free software; you can redistribute it and/or modify    *
+ * it under the terms of the GNU General Public License as published by    *
+ * the Free Software Foundation; either version 3 of the License, or       *
+ * (at your option) any later version.                                     *
+ *                                                                         *
+ * This program is distributed in the hope that it will be useful,         *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ * GNU General Public License for more details.                            *
+ *                                                                         *
+ * You should have received a copy of the GNU General Public License       *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
+ **************************************************************************/
+
+#include "downloadbar.h"
+#include "ui_downloadbar.h"
+
+updateDownloadDialog::updateDownloadDialog(QWidget* parent)
+    : QDialog(parent)
+    , ui(new Ui::updateDownloadDialog)
+{
+    ui->setupUi(this);
+    ui->progressBar->setValue(0);
+    ui->progressBar->setVisible(true);
+    setWindowFlags(windowFlags() & ~Qt::WindowContextHelpButtonHint);
+}
+
+void updateDownloadDialog::setValue(double var)
+{
+    value_ = var;
+}
+
+void updateDownloadDialog::setMaximum(double var)
+{
+    maximum_ = var;
+}
+
+updateDownloadDialog::~updateDownloadDialog()
+{
+    delete ui;
+}
+
+void updateDownloadDialog::update(QString displayText)
+{
+    int percent = static_cast<int>((1 - ((maximum_ - value_) / maximum_)) * 100);
+    ui->Statusedit->setText("Speed: " + displayText);
+    ui->progressBar->setValue(percent);
+}
