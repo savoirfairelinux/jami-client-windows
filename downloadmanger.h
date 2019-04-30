55bfe79601068c102a9a1a42f657a1863a8ebfb8
Ming Rui Zhang <mingrui.zhang@savoirfairelinux.com>
2019-04-30 16:30:24 -0400

    updatelogic: add qt download manger to handle Jami version verification, and new installer download. add new ui to handle user update event
    Change-Id: Id71232a8c32675391f9d46b0b5a7658e8a47fc95

---
+++ downloadmanger.h
@@ -0,0 +1,50 @@
+/***************************************************************************
+ * Copyright (C) 2015-2019 by Savoir-faire Linux                           *
+ * Author: Mingrui Zhang <mingrui.zhang@savoirfairelinux.com>              *
+ *                                                                         *
+ * This program is free software; you can redistribute it and/or modify    *
+ * it under the terms of the GNU General Public License as published by    *
+ * the Free Software Foundation; either version 3 of the License, or       *
+ * (at your option) any later version.                                     *
+ *                                                                         *
+ * This program is distributed in the hope that it will be useful,         *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ * GNU General Public License for more details.                            *
+ *                                                                         *
+ * You should have received a copy of the GNU General Public License       *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
+ **************************************************************************/
+
+#pragma once
+
+#include <memory>
+
+#include <QtCore>
+#include <QtNetwork>
+
+#include "downloadbar.h"
+
+class DownloadManager : public QObject {
+    Q_OBJECT
+    QNetworkAccessManager manager_;
+    QNetworkReply* currentDownload_;
+    updateDownloadDialog probar_;
+    std::unique_ptr<QFile> file_;
+    QTime downloadTime_;
+    int previousTime_ = 0;
+    qint64 previousDownloadBytes_ = 0;
+    QString downloadpath_;
+
+    const char* WinGetEnv(const char* name);
+
+public:
+    explicit DownloadManager(QObject* parent = nullptr);
+    void doDownload(const QUrl& url);
+    QString versionOnline();
+
+public slots:
+    void downloadFinished();
+    void downloadProgress(qint64 bytesRead, qint64 totalBytes);
+    void httpReadyRead();
+};
