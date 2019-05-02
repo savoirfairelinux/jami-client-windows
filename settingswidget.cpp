8a8a70032fec7a7b4b9e8e6f4a0778f191ce3c62
Ming Rui Zhang <mingrui.zhang@savoirfairelinux.com>
2019-05-02 17:27:08 -0400

    updatemechanism: modify Product.wxs to fix Add/Remove Icon bug, correctify the VERSION_STRING to be extended into hour, and make sure the update mechanism works as expected
    Change-Id: I5fa9db19f27d368b359d4109e3be71f060f9bf63

--- settingswidget.cpp
+++ settingswidget.cpp
@@ -44,6 +44,7 @@
 #include "deviceitemwidget.h"
 #include "banneditemwidget.h"
 #include "updateconfimdialog.h"
+#include "version.h"
 
 #include "api/newdevicemodel.h"
 #include "audio/inputdevicemodel.h"
@@ -291,12 +292,11 @@ void SettingsWidget::updateAccountInfoDisplayed()
     ui->bannedContactsLayoutWidget->setVisible(accInfo.contactModel->getBannedContacts().size());
 }
 
-void
-SettingsWidget::setAvatar(PhotoboothWidget* avatarWidget)
+void SettingsWidget::setAvatar(PhotoboothWidget* avatarWidget)
 {
     auto& accountInfo = LRCInstance::getCurrentAccountInfo();
     auto defaultAvatar = accountInfo.profileInfo.avatar.empty();
-    auto avatar = Utils::accountPhoto(accountInfo, { avatarSize_ , avatarSize_ });
+    auto avatar = Utils::accountPhoto(accountInfo, {avatarSize_, avatarSize_});
     avatarWidget->setAvatarPixmap(QPixmap::fromImage(avatar), defaultAvatar);
 }
 
@@ -484,8 +484,7 @@ void SettingsWidget::setButtonIconSlot(int frame)
     ui->regNameButton->setIcon(QIcon(gif->currentPixmap()));
 }
 
-void
-SettingsWidget::slotRegisterName()
+void SettingsWidget::slotRegisterName()
 {
     if (!regNameBtn_) {
         return;
@@ -517,35 +516,27 @@ void SettingsWidget::setAccEnableSlot(int state)
 void SettingsWidget::delAccountSlot()
 {
     DownloadManager manager;
-    UpdateConfimDialog updatedia;
-
-    QFile file("Version.txt");
-    if (!file.open(QIODevice::ReadOnly)) {
-        QMessageBox::critical(this, "Version File Error", file.errorString());
-    }
-    QTextStream in(&file);
-    QString localVersion = in.readLine();
-    file.close();
+    UpdateConfimDialog updatedia(this);
 
     QString onlineVersion = manager.versionOnline();
-    if (onlineVersion != localVersion) {
+    if (onlineVersion != QString(VERSION_STRING) && onlineVersion != "Null") {
         auto ret = updatedia.exec();
         QThread::sleep(1);
         if (ret == QDialog::Accepted) {
-            QString urlstr = "https://dl.jami.net/windows/test.exe";
+            QString urlstr = "https://dl.jami.net/windows/Test/Jami.msi";
             QUrl url = QUrl::fromEncoded(urlstr.toLocal8Bit());
             manager.doDownload(url);
             auto args = QString(" /passive /norestart WIXNONUILAUNCH=1");
-            auto dir = QString("C:\\Users\\mzhang\\Desktop");
-            auto cmd = "powershell " + dir + "\\Jami.msi"
-                + " /L*V " + dir + "\\jami_install.log" + args;
+            auto dir = DownloadManager::WinGetEnv("TEMP");
+            auto cmd = "powershell " + QString(dir) + "\\Jami.msi"
+                + " /L*V " + QString(dir) + "\\jami_install.log" + args;
             auto retq = QProcess::startDetached(cmd);
             if (retq) {
                 QApplication::quit();
             }
         }
     } else {
-        QMessageBox::information(this, "Update Status", "No New Version Detected");
+        QMessageBox::information(0, "Update Status", "No New Version Detected");
     }
 
     /*DeleteAccountDialog delDialog(this);
@@ -639,11 +630,11 @@ void SettingsWidget::updateAndShowDevicesSlot()
         auto item = new QListWidgetItem(ui->settingsListWidget);
         ui->settingsListWidget->addItem(item);
         auto row = new DeviceItemWidget(QString::fromStdString(it->name),
-                                        QString::fromStdString(it->id),
-                                        it->isCurrent,
-                                        this);
+            QString::fromStdString(it->id),
+            it->isCurrent,
+            this);
         item->setSizeHint(QSize(ui->settingsListWidget->width(),
-                                row->minimumSizeHint().height()));
+            row->minimumSizeHint().height()));
         ui->settingsListWidget->setItemWidget(item, row);
         connect(row, &DeviceItemWidget::btnRemoveDeviceClicked,
             [this, i, isCurrent = it->isCurrent]() {
@@ -679,10 +670,10 @@ void SettingsWidget::updateAndShowBannedContactsSlot()
         auto item = new QListWidgetItem(ui->bannedListWidget);
         ui->bannedListWidget->addItem(item);
         auto row = new BannedItemWidget(contactName,
-                                        contactId,
-                                        this);
+            contactId,
+            this);
         item->setSizeHint(QSize(ui->bannedListWidget->width(),
-                                row->minimumSizeHint().height()));
+            row->minimumSizeHint().height()));
         ui->bannedListWidget->setItemWidget(item, row);
         connect(row, &BannedItemWidget::btnReAddContactClicked,
             [this, i]() {
