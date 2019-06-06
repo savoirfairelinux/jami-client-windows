dd3b6d5e7b72d8a95a7113a08c5d2a58d6ea0679
Andreas Traczyk <andreas.traczyk@savoirfairelinux.com>
2019-06-06 17:38:52 -0400

    mainwindow: detect secondary instances of Jami and restore primary
    - connects a qt socket to the primary instance running a local
      server to inform the primary instance that it should restore
      from a possibly minimized state
    
    Change-Id: Iceda1a5a8be9fda2d34d41d9d32b6c02f26d06e3
    Gitlab: #480

--- runguard.cpp
+++ runguard.cpp
@@ -1,8 +1,27 @@
-// From: https://stackoverflow.com/a/28172162
+/***************************************************************************
+ * Copyright (C) 2019 by Savoir-faire Linux                                *
+ * Author: Andreas Traczyk <andreas.traczyk@savoirfairelinux.com>          *
+ *                                                                         *
+ * This program is free software; you can redistribute it and/or modify    *
+ * it under the terms of the GNU General Public License as published by    *
+ * the Free Software Foundation; either version 3 of the License, or       *
+ * (at your option) any later version.                                     *
+ *                                                                         *
+ * This program is distributed in the hope that it will be useful,         *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ * GNU General Public License for more details.                            *
+ *                                                                         *
+ * You should have received a copy of the GNU General Public License       *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
+ **************************************************************************/
+ // Based on: https://stackoverflow.com/a/28172162
 
 #include "runguard.h"
 
 #include <QCryptographicHash>
+
+#include "mainwindow.h"
 
 namespace
 {
@@ -21,59 +40,85 @@ QString generateKeyHash(const QString& key, const QString& salt)
 }
 
 RunGuard::RunGuard(const QString& key)
-    : key(key)
-    , memLockKey(generateKeyHash(key, "_memLockKey"))
-    , sharedmemKey(generateKeyHash(key, "_sharedmemKey"))
-    , sharedMem(sharedmemKey)
-    , memLock(memLockKey, 1)
-{
-    memLock.acquire();
-    {
-        QSharedMemory fix(sharedmemKey);    // Fix for *nix: http://habrahabr.ru/post/173281/
-        fix.attach();
-    }
-    memLock.release();
-}
+    : key_(key)
+    , memLockKey_(generateKeyHash(key, "_memLockKey"))
+    , sharedmemKey_(generateKeyHash(key, "_sharedmemKey"))
+    , sharedMem_(sharedmemKey_)
+    , memLock_(memLockKey_, 1)
+{}
 
 RunGuard::~RunGuard()
 {
     release();
 }
 
+void
+RunGuard::tryRestorePrimaryInstance()
+{
+    MainWindow::instance().showWindow();
+}
+
 bool RunGuard::isAnotherRunning()
 {
-    if (sharedMem.isAttached())
+    if (sharedMem_.isAttached())
         return false;
 
-    memLock.acquire();
-    const bool isRunning = sharedMem.attach();
+    memLock_.acquire();
+    const bool isRunning = sharedMem_.attach();
     if (isRunning)
-        sharedMem.detach();
-    memLock.release();
+        sharedMem_.detach();
+    memLock_.release();
 
     return isRunning;
 }
 
 bool RunGuard::tryToRun()
 {
-    if (isAnotherRunning())   // Extra check
+    if (isAnotherRunning()) {
+        // This is a secondary instance,
+        // connect to the primary instance to trigger a restore
+        // then fail.
+        if (socket_ == nullptr) {
+            socket_ = new QLocalSocket();
+        }
+        if (socket_->state() == QLocalSocket::UnconnectedState ||
+            socket_->state() == QLocalSocket::ClosingState) {
+            socket_->connectToServer(key_);
+        }
+        if (socket_->state() == QLocalSocket::ConnectingState) {
+            socket_->waitForConnected();
+        }
         return false;
+    }
 
-    memLock.acquire();
-    const bool result = sharedMem.create(sizeof(quint64));
-    memLock.release();
+    memLock_.acquire();
+    const bool result = sharedMem_.create(sizeof(quint64));
+    memLock_.release();
     if (!result) {
         release();
         return false;
     }
+
+    // This is the primary instance,
+    // listen for subsequent instances.
+    QLocalServer::removeServer(key_);
+    server_ = new QLocalServer();
+    server_->setSocketOptions(QLocalServer::UserAccessOption);
+    server_->listen(key_);
+    QObject::connect(
+        server_,
+        &QLocalServer::newConnection,
+        this,
+        &RunGuard::tryRestorePrimaryInstance
+    );
 
     return true;
 }
 
 void RunGuard::release()
 {
-    memLock.acquire();
-    if (sharedMem.isAttached())
-        sharedMem.detach();
-    memLock.release();
+    memLock_.acquire();
+    if (sharedMem_.isAttached())
+        sharedMem_.detach();
+    memLock_.release();
 }
