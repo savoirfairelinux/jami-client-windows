d5930861a6810d022b2e84c8145225019181bb25
Andreas Traczyk <andreas.traczyk@savoirfairelinux.com>
2019-06-10 15:31:09 -0400

    packaging: remove old installations
    Change-Id: If5c4e6c95244560b62f1eec985583cf04b7edd5e

--- utils.cpp
+++ utils.cpp
@@ -49,7 +49,7 @@
 #include "version.h"
 
 bool
-Utils::CreateStartupLink()
+Utils::CreateStartupLink(const std::wstring& wstrAppName)
 {
 #ifdef Q_OS_WIN
     TCHAR szPath[MAX_PATH];
@@ -61,7 +61,7 @@ Utils::CreateStartupLink()
     SHGetFolderPathW(NULL, CSIDL_STARTUP, NULL, 0, startupPath);
 
     std::wstring linkPath(startupPath);
-    linkPath += TEXT("\\Jami.lnk");
+    linkPath += std::wstring(TEXT("\\") + wstrAppName + TEXT(".lnk"));
 
     return Utils::CreateLink(programPath.c_str(), linkPath.c_str());
 #else
@@ -100,29 +100,79 @@ Utils::CreateLink(LPCWSTR lpszPathObj, LPCWSTR lpszPathLink) {
 }
 
 void
-Utils::DeleteStartupLink() {
+Utils::DeleteStartupLink(const std::wstring& wstrAppName) {
 #ifdef Q_OS_WIN
     TCHAR startupPath[MAX_PATH];
     SHGetFolderPathW(NULL, CSIDL_STARTUP, NULL, 0, startupPath);
 
     std::wstring linkPath(startupPath);
-    linkPath += TEXT("\\Jami.lnk");
+    linkPath += std::wstring(TEXT("\\") + wstrAppName + TEXT(".lnk"));
 
     DeleteFile(linkPath.c_str());
 #endif
 }
 
 bool
-Utils::CheckStartupLink() {
+Utils::CheckStartupLink(const std::wstring& wstrAppName) {
 #ifdef Q_OS_WIN
     TCHAR startupPath[MAX_PATH];
     SHGetFolderPathW(NULL, CSIDL_STARTUP, NULL, 0, startupPath);
 
     std::wstring linkPath(startupPath);
-    linkPath += TEXT("\\Jami.lnk");
+    linkPath += std::wstring(TEXT("\\") + wstrAppName + TEXT(".lnk"));
     return PathFileExists(linkPath.c_str());
 #else
     return true;
+#endif
+}
+
+const char*
+Utils::WinGetEnv(const char* name)
+{
+#ifdef Q_OS_WIN
+    const DWORD buffSize = 65535;
+    static char buffer[buffSize];
+    if (GetEnvironmentVariableA(name, buffer, buffSize)) {
+        return buffer;
+    } else {
+        return 0;
+    }
+#else
+    return 0;
+#endif
+}
+
+void
+Utils::removeOldVersions()
+{
+#ifdef Q_OS_WIN
+    // As per: https://git.ring.cx/savoirfairelinux/ring-client-windows/issues/429
+    // NB: As only the 64-bit version of this application is distributed, we will only
+    // remove 1. configuration reg keys for Ring-x64, 2. startup links for Ring,
+    // 3. winsparkle reg keys, and 4. NSIS uninstall reg keys for Jami-x64.
+    // Uninstallation of Ring, either 32 or 64 bit, is up to the user to complete.
+    // However, the current version of Jami will now kill Ring.exe upon start.
+    QString node64 = "HKEY_LOCAL_MACHINE\\SOFTWARE\\WOW6432Node";
+    QString hkcuSoftwareKey = "HKEY_CURRENT_USER\\Software\\";
+    QString uninstKey = "\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\";
+    QString company = "Savoir-Faire Linux";
+
+    // 1. configuration reg keys for Ring-x64
+    QSettings(hkcuSoftwareKey + "jami.net\\Ring", QSettings::NativeFormat).remove("");
+    QSettings(hkcuSoftwareKey + "ring.cx", QSettings::NativeFormat).remove("");
+    // 2. unset Ring as a startup application
+    if (Utils::CheckStartupLink(TEXT("Ring"))) {
+        qDebug() << "Found startup link for Ring. Removing it and killing Ring.exe.";
+        Utils::DeleteStartupLink(TEXT("Ring"));
+        QProcess::execute("taskkill /im Ring.exe /f");
+    }
+    // 3. remove registry entries for winsparkle(both Jami-x64 and Ring-x64)
+    QSettings(hkcuSoftwareKey + company, QSettings::NativeFormat).remove("");
+    // 4. remove registry entries for Jami-x64
+    QSettings(node64 + uninstKey + company + " " + "Jami", QSettings::NativeFormat).remove("");
+
+#else
+    return;
 #endif
 }
 
@@ -234,18 +284,6 @@ void Utils::showSystemNotification(QWidget* widget,
     GlobalSystemTray::instance()
         .showMessage(sender, message, QIcon(":images/jami.png"));
     QApplication::alert(widget, delay);
-}
-
-const char*
-Utils::WinGetEnv(const char* name)
-{
-    const DWORD buffSize = 65535;
-    static char buffer[buffSize];
-    if (GetEnvironmentVariableA(name, buffer, buffSize)) {
-        return buffer;
-    } else {
-        return 0;
-    }
 }
 
 void
