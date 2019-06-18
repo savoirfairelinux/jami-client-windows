46dff77ffc312f5f01e94ddcbdd23331c12babdb
Ming Rui Zhang <mingrui.zhang@savoirfairelinux.com>
2019-06-18 15:05:19 -0400

    messagewebview: add file&image sendarea
    -can contain file and image
    -can add directly through the plus button
    
    Change-Id: I191aa7c4ecd908c28af47d93f7602925a129f77c

--- messagewebview.h
+++ messagewebview.h
@@ -39,6 +39,7 @@ public:
     Q_INVOKABLE int acceptFile(const QString& arg);
     Q_INVOKABLE int refuseFile(const QString& arg);
     Q_INVOKABLE int sendMessage(const QString& arg);
+    Q_INVOKABLE int sendImage(const QString& arg);
     Q_INVOKABLE int sendFile();
     Q_INVOKABLE int log(const QString& arg);
     Q_INVOKABLE int acceptInvitation();
@@ -78,8 +79,9 @@ public:
                        const std::string& contactUri = "",
                        const std::string& contactId = "");
     void setMessagesVisibility(bool visible);
-    void setMessagesContent(QString text);
+    void setMessagesContent(const QString &text);
     void copySelectedText(QClipboard* clipboard);
+    void setMessagesImageContent(const QString &text, const short& type);
     bool textSelected();
     void runJsText();
 
