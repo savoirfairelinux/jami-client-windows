43e5d548cb34c3761d963494e5f6364fc6b448bd
Ming Rui Zhang <mingrui.zhang@savoirfairelinux.com>
2019-07-02 10:51:06 -0400

    callwidget: add sipcall status code notification
    Change-Id: I80a30e1ed4a8ad732ca84fcf031434d024c50f43

--- callwidget.cpp
+++ callwidget.cpp
@@ -27,6 +27,7 @@
 #include <QDesktopServices>
 #include <QScrollBar>
 #include <QWebEngineScript>
+#include <QMessagebox>
 
 #include <algorithm>
 #include <memory>
@@ -1218,7 +1219,7 @@ CallWidget::connectAccount(const std::string& accId)
     auto callModel = LRCInstance::accountModel().getAccountInfo(accId).callModel.get();
     disconnect(callStatusChangedConnection_);
     callStatusChangedConnection_ = QObject::connect(callModel, &lrc::api::NewCallModel::callStatusChanged,
-        [this, accId](const std::string& callId) {
+        [this, accId](const std::string& callId, int code) {
             auto callModel = LRCInstance::accountModel().getAccountInfo(accId).callModel.get();
             auto call = callModel->getCall(callId);
             switch (call.status) {
@@ -1231,6 +1232,10 @@ CallWidget::connectAccount(const std::string& accId)
             {
                 setCallPanelVisibility(false);
                 showConversationView();
+                //Failure and hung up in terminatting
+                if (LRCInstance::getCurrentAccountInfo().profileInfo.type == lrc::api::profile::Type::SIP && code > 202) {
+                    Utils::showSystemNotification(this, QString::fromStdString(lrc::api::NewCallModel::getSIPCallStatusString(code)), QString::number(code), 10000);
+                }
                 break;
             }
             default:
