b8a233b6691e14dd1e6bd37e899436d44a6ca1e6
Ming Rui Zhang <mingrui.zhang@savoirfairelinux.com>
2019-07-02 10:51:06 -0400

    callwidget: add sipcall status code notification
    Change-Id: I80a30e1ed4a8ad732ca84fcf031434d024c50f43

--- callwidget.cpp
+++ callwidget.cpp
@@ -1218,7 +1218,7 @@ CallWidget::connectAccount(const std::string& accId)
     auto callModel = LRCInstance::accountModel().getAccountInfo(accId).callModel.get();
     disconnect(callStatusChangedConnection_);
     callStatusChangedConnection_ = QObject::connect(callModel, &lrc::api::NewCallModel::callStatusChanged,
-        [this, accId](const std::string& callId) {
+        [this, accId](const std::string& callId, int code) {
             auto callModel = LRCInstance::accountModel().getAccountInfo(accId).callModel.get();
             auto call = callModel->getCall(callId);
             switch (call.status) {
@@ -1231,6 +1231,22 @@ CallWidget::connectAccount(const std::string& accId)
             {
                 setCallPanelVisibility(false);
                 showConversationView();
+                //Failure and hung up in terminatting
+                if (code == 0)
+                    break;
+                if (LRCInstance::getCurrentAccountInfo().profileInfo.type == lrc::api::profile::Type::SIP && code >= 400) {
+                    Utils::showSystemNotification(this, QString::fromStdString(lrc::api::NewCallModel::getSIPCallStatusString(code)), "code: " + QString::number(code), 10000);
+                    break;
+                } else if (LRCInstance::getCurrentAccountInfo().profileInfo.type == lrc::api::profile::Type::RING) {
+                    if (code < 180 && code != ECONNABORTED && code != ECONNREFUSED) {
+                        std::error_condition c (code,std::generic_category());
+                        if(c.message() != "")
+                            Utils::showSystemNotification(this, QString::fromStdString(c.message()), "code: " + QString::number(code), 10000);
+                        break;
+                    } else if (code >= 400) {
+                        Utils::showSystemNotification(this, QString::fromStdString(lrc::api::NewCallModel::getSIPCallStatusString(code)), "code: " + QString::number(code), 10000);
+                    }
+                }
                 break;
             }
             default:
