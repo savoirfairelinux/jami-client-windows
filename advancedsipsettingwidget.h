f9c0fcd76bd391878a2fa5fc7d1ee9fdbab35476
Ming Rui Zhang <mingrui.zhang@savoirfairelinux.com>
2019-08-12 17:13:29 -0400

    advancedSIPSetting: add published addresses section into sip settings
    Change-Id: I9bd5a3d50694b0226d1743c7b474d02d29396122

--- advancedsipsettingwidget.h
+++ advancedsipsettingwidget.h
@@ -85,6 +85,12 @@ private slots:
     void setTURNRealm();
     void setSTUNAddress();
 
+    // published address
+    void setUseCustomAddressAndPort(bool state);
+
+    void lineEditSIPCustomAddressLineEditTextChanged(const QString& text);
+    void customPortSIPSpinBoxValueChanged(const int& value);
+
     // codecs
     void updateAudioCodecs();
     void updateVideoCodecs();
