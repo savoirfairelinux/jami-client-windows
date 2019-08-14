3426e78abb3bf7785eb3938e1035d72ca5429808
Yang Wang <yang.wang@savoirfairelinux.com>
2019-08-14 12:52:43 -0400

    Magnetic snap
    - remove resize
    - re-implement paint event
    - the preview now stays when the user set it in the whole cession
    
    Change-Id: I7d57f8de01d8db171c380109bfbe8295cd1378a0
    Gitlab: #495

--- videoview.cpp
+++ videoview.cpp
@@ -359,23 +359,43 @@ void VideoView::mousePressEvent(QMouseEvent* event)
     QPoint clickPosition = event->pos();
     if (ui->videoWidget->getPreviewRect().contains(clickPosition)) {
         QLine distance = QLine(clickPosition, ui->videoWidget->getPreviewRect().bottomRight());
-        if (distance.dy() < resizeGrip_ and distance.dx() < resizeGrip_) {
-            QApplication::setOverrideCursor(Qt::SizeFDiagCursor);
-            resizingPreview_ = true;
-        } else {
             originMouseDisplacement_ = event->pos() - ui->videoWidget->getPreviewRect().topLeft();
             QApplication::setOverrideCursor(Qt::SizeAllCursor);
             draggingPreview_ = true;
-        }
     }
 }
 
 void VideoView::mouseReleaseEvent(QMouseEvent* event)
 {
     Q_UNUSED(event)
+    if (draggingPreview_) {
+		//Check preview's current central position
+		QRect& previewRect = ui->videoWidget->getPreviewRect();
+		auto previewCentral = previewRect.center();
+		auto videoViewRect = ui->videoWidget->rect();
+		auto videoWidgetCentral = videoViewRect.center();
+		if (previewCentral.x() >= videoWidgetCentral.x()) {
+			if (previewCentral.y() >= videoWidgetCentral.y())
+			{
+				//Move preview to bottom right
+				ui->videoWidget->movePreview(VideoWidget::TargetPointPreview::bottomRight);
+			} else {
+				//Move preview to top right
+				ui->videoWidget->movePreview(VideoWidget::TargetPointPreview::topRight);
+
+			}
+		} else {
+			if (previewCentral.y() >= videoWidgetCentral.y()) {
+				//Move preview to bottom left
+				ui->videoWidget->movePreview(VideoWidget::TargetPointPreview::bottomLeft);
+			} else {
+				//Move preview to top left
+				ui->videoWidget->movePreview(VideoWidget::TargetPointPreview::topLeft);
+			}
+		}
+    }
 
     draggingPreview_ = false;
-    resizingPreview_ = false;
     QApplication::setOverrideCursor(Qt::ArrowCursor);
 }
 
