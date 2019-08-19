e1f49e02e9817fadf66e01f6856b9c3ae4d446fb
Yang Wang <yang.wang@savoirfairelinux.com>
2019-08-19 14:47:40 -0400

    videoview: audio only overlay created
    - add avatar view for audio only overlay, in which the avatar photo, id and name of
      the contact are displayed
    
    Change-Id: I507ed064208eeb12a735897a7540caa1547c0aed
    Gitlab: #495

--- videooverlay.h
+++ videooverlay.h
@@ -48,14 +48,14 @@ public:
     bool getShowChatView();
     void setTransferCallAndSIPPanelAvailability(bool visible);
     void setCurrentSelectedCalleeDisplayName(const QString& CalleeDisplayName);
-    void resetOverlay(bool isAudioMuted, bool isVideoMuted, bool isRecording, bool isHolding);
+    void resetOverlay(bool isAudioMuted, bool isVideoMuted, bool isRecording, bool isHolding, bool isAudioOnly);
 
 //UI SLOTS
 private slots:
     void setTime();
     void on_hangupButton_clicked();
     void on_chatButton_toggled(bool checked);
-    void on_holdButton_clicked();
+    void on_holdButton_toggled(bool checked);
     void on_noMicButton_toggled(bool checked);
     void on_noVideoButton_toggled(bool checked);
     void on_recButton_clicked();
@@ -74,4 +74,6 @@ private:
 
 signals:
     void setChatVisibility(bool visible);
+    void HoldStatusChanged(bool pauseLabelStatus);
+
 };
