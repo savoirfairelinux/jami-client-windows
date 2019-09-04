bff348cd5b6c5da147bd24c15cd4838182c69ada
Ming Rui Zhang <mingrui.zhang@savoirfairelinux.com>
2019-09-04 17:15:51 -0400

    videoview: seperation of preview renderer and distant renderer
    Change-Id: I9af9e0a4bb07a83b68647015dc8a4e2a21cd2170

--- callwidget.h
+++ callwidget.h
@@ -32,6 +32,7 @@
 
 #include "navwidget.h"
 #include "smartlistmodel.h"
+#include "previewrender.h"
 
 // new LRC
 #include "api/account.h"
@@ -58,10 +59,7 @@ public:
     ~CallWidget();
 
     int getLeftPanelWidth();
-    void disconnectRendering();
-
-    // if started is true, only update, stop signals are connected
-    void connectRendering(bool started = false);
+    void reconnectRenderingVideoDeviceChanged();
 
     // NavWidget
     virtual void navigated(bool to);
@@ -143,11 +141,14 @@ private:
 
     QMenu* menu_;
     QClipboard* clipboard_;
+    PreviewRenderWidget* previewRenderer_;
 
     Ui::CallWidget* ui;
     QMovie* miniSpinner_;
 
     constexpr static int qrSize_ = 200;
+    constexpr static int previewCallWidgetWidth_ = 150;
+    constexpr static int previewCallWidgetHeight_ = 110;
 
     // lrc
     Video::Renderer* videoRenderer_;
