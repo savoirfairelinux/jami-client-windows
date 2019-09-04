76eab9bd94d2e5cd976cf684b5bf0f5859ea2f49
Ming Rui Zhang <mingrui.zhang@savoirfairelinux.com>
2019-09-04 17:15:51 -0400

    videoview: seperation of preview renderer and distant renderer
    Change-Id: I9af9e0a4bb07a83b68647015dc8a4e2a21cd2170

--- utils.cpp
+++ utils.cpp
@@ -30,6 +30,7 @@
 #endif
 
 //Qt
+#include <QBitmap>
 #include <QObject>
 #include <QErrorMessage>
 #include <QPainter>
@@ -259,6 +260,47 @@ Utils::getCirclePhoto(const QImage original, int sizePhoto)
     painter.setCompositionMode(QPainter::CompositionMode_SourceIn);
     painter.drawImage(0, 0, scaledPhoto, margin, 0);
     return target;
+}
+
+QImage
+Utils::getRoundedEdgePhoto(const QImage original, int widthPhoto, int heightPhoto, int roundness)
+{
+    // First, create a transparent image with the same size
+    QImage target(widthPhoto, heightPhoto, QImage::Format_ARGB32_Premultiplied);
+    target.fill(Qt::transparent);
+
+    //Second, create a painter onto that image
+    QPainter painter(&target);
+    painter.setRenderHints(QPainter::Antialiasing | QPainter::SmoothPixmapTransform);
+    painter.setBrush(QBrush(original));
+    // set brush to the image that we want to draw
+    // Note that, the Qbrush is in default texture mode
+
+    //Third, scale the original image into the size of the image we created
+    auto scaledPhoto = original
+            .scaled(widthPhoto, heightPhoto, Qt::KeepAspectRatioByExpanding, Qt::SmoothTransformation)
+            .convertToFormat(QImage::Format_ARGB32_Premultiplied);
+    painter.drawRoundedRect(0, 0, widthPhoto, heightPhoto, roundness, roundness);
+    painter.setCompositionMode(QPainter::CompositionMode_SourceIn);
+    // Source-In mode and Draw the image onto the transperant image
+    painter.drawImage(0, 0, scaledPhoto, 0, 0);
+    return target;
+}
+
+void
+Utils::drawBlackCircularImageOntoLabel(QLabel* containerWidget)
+{
+    // Widget is black, fill image with white
+    // draw a black cycle onto it
+    QImage target(containerWidget->width(), containerWidget->height(), QImage::Format_ARGB32_Premultiplied);
+    target.fill(Qt::white);
+
+    QPainter painter(&target);
+    painter.setRenderHints(QPainter::Antialiasing | QPainter::SmoothPixmapTransform);
+    painter.setBrush(QBrush(Qt::black));
+    painter.drawEllipse(containerWidget->x(), containerWidget->y(), containerWidget->width(), containerWidget->height());
+    painter.setCompositionMode(QPainter::CompositionMode_SourceIn);
+    containerWidget->setPixmap(QPixmap::fromImage(target));
 }
 
 void
