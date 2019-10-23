fc550b0fac8c41e66f1494d5cb6751c73faeb2a1
Ming Rui Zhang <mingrui.zhang@savoirfairelinux.com>
2019-10-23 16:24:40 -0400

    restruct: remove unused files and change file arrangement
    Change-Id: Ia91c7988403feb2647653d88dca0c4780a734b3d

---
+++ src/dialogs/accountmigrationdialog.h
@@ -0,0 +1,54 @@
+/***************************************************************************
+ * Copyright (C) 2015-2019 by Savoir-faire Linux                           *
+ * Author: Mingrui Zhang <mingrui.zhang@savoirfairelinux.com>              *
+ *                                                                         *
+ * This program is free software; you can redistribute it and/or modify    *
+ * it under the terms of the GNU General Public License as published by    *
+ * the Free Software Foundation; either version 3 of the License, or       *
+ * (at your option) any later version.                                     *
+ *                                                                         *
+ * This program is distributed in the hope that it will be useful,         *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ * GNU General Public License for more details.                            *
+ *                                                                         *
+ * You should have received a copy of the GNU General Public License       *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
+ **************************************************************************/
+
+#pragma once
+
+#include <QDialog>
+#include <QMovie>
+
+#include "../misc/utils.h"
+#include "../bridging//lrcinstance.h"
+
+namespace Ui {
+    class AccountMigrationDialog;
+}
+
+class AccountMigrationDialog : public QDialog
+{
+    Q_OBJECT;
+
+public:
+
+    explicit AccountMigrationDialog(QWidget *parent = 0, const std::string& accountId = {});
+    ~AccountMigrationDialog();
+
+private:
+    Ui::AccountMigrationDialog *ui;
+
+    std::string accountId_;
+    std::string password_;
+    QMovie* migrationSpinnerMovie_;
+
+private slots:
+    void slotPasswordEditingFinished();
+    void slotDeleteButtonClicked();
+    void slotMigrationButtonClicked();
+
+protected:
+    void closeEvent(QCloseEvent *event);
+};
