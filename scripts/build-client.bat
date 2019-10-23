5bf64c5fb3b5dc07a35d5643b2b062de6bc2adcc
Ming Rui Zhang <mingrui.zhang@savoirfairelinux.com>
2019-10-23 16:24:40 -0400

    restruct: remove unused files and change file arrangement
    Change-Id: Ia91c7988403feb2647653d88dca0c4780a734b3d

---
+++ scripts/build-client.bat
@@ -0,0 +1,137 @@
+:: Jami - native Windows client project build script
+
+@echo off
+setlocal
+
+if "%1" == "/?" goto Usage
+if "%~1" == "" goto Usage
+
+set doDeps=N
+set doCompile=N
+set doBuild=N
+set doBuildBeta=N
+
+set SCRIPTNAME=%~nx0
+
+if "%1"=="compile" (
+    set doCompile=Y
+) else if "%1"=="build" (
+    set doBuild=Y
+) else if "%1"=="deps" (
+    set doDeps=Y
+) else if "%1"=="beta" (
+    set doBuildBeta=Y
+) else (
+    goto Usage
+)
+
+set arch=N
+
+shift
+:ParseArgs
+if "%1" == "" goto FinishedArgs
+if /I "%1"=="x86" (
+    set arch=x86
+) else if /I "%1"=="x64" (
+    set arch=x64
+) else (
+    goto Usage
+)
+shift
+goto ParseArgs
+
+:FinishedArgs
+if "%arch%"=="x86" (
+    set MSBUILD_ARGS=/nologo /p:useenv=true /p:Platform=Win32 /maxcpucount:%NUMBER_OF_PROCESSORS%
+) else if "%arch%"=="x64" (
+    set MSBUILD_ARGS=/nologo /p:useenv=true /p:Platform=x64 /maxcpucount:%NUMBER_OF_PROCESSORS%
+)
+
+@setlocal
+
+set VSInstallerFolder="%ProgramFiles(x86)%\Microsoft Visual Studio\Installer"
+if %PROCESSOR_ARCHITECTURE%==x86 set VSInstallerFolder="%ProgramFiles%\Microsoft Visual Studio\Installer"
+
+pushd %VSInstallerFolder%
+for /f "usebackq tokens=*" %%i in (`vswhere -latest -products * -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationPath`) do (
+  set VSLATESTDIR=%%i
+)
+popd
+
+echo VS Installation folder: %VSLATESTDIR%
+
+if not exist "%VSLATESTDIR%\VC\Auxiliary\Build\vcvarsall.bat" (
+    echo:
+    echo VSInstallDir not found or not installed correctly.
+    goto cleanup
+)
+
+if %PROCESSOR_ARCHITECTURE%==x86 (
+    set Comp_x86=x86 10.0.15063.0
+    set Comp_x64=x86_amd64 10.0.15063.0
+) else (
+    set Comp_x86=amd64_x86 10.0.15063.0
+    set Comp_x64=amd64 10.0.15063.0
+)
+
+set path=%path:"=%
+if "%arch%"=="x86" (
+    call "%VSLATESTDIR%"\\VC\\Auxiliary\\Build\\vcvarsall.bat %Comp_x86%
+) else if "%arch%"=="x64" (
+    call "%VSLATESTDIR%"\\VC\\Auxiliary\\Build\\vcvarsall.bat %Comp_x64%
+)
+
+if "%arch%" neq "N" (
+    if "%doCompile%" neq "N" (
+        goto compileClient
+    ) else if "%doBuild%" neq "N" (
+        goto buildClient
+    ) else if "%doDeps%" neq "N" (
+        goto buildDeps
+    ) else if "%doBuildBeta%" neq "N" (
+        goto buildBetaClient
+    )
+    goto :eof
+)
+goto Usage
+
+:buildDeps
+set TOBUILD=..\qrencode-win32\qrencode-win32\vc8\qrcodelib\qrcodelib.vcxproj
+msbuild %TOBUILD% /verbosity:normal /p:Configuration=Release-Lib %MSBUILD_ARGS%
+set WGET_CMD=wget --no-check-certificate --retry-connrefused --waitretry=1 --read-timeout=20 --timeout=15 --tries=4
+goto cleanup
+
+:compileClient
+msbuild ring-client-windows.vcxproj /verbosity:normal /p:Configuration=ReleaseCompile %MSBUILD_ARGS%
+goto cleanup
+
+:buildClient
+msbuild ring-client-windows.vcxproj /verbosity:normal /p:Configuration=Release %MSBUILD_ARGS%
+goto cleanup
+
+:buildBetaClient
+msbuild ring-client-windows.vcxproj /verbosity:normal /p:Configuration=Beta %MSBUILD_ARGS%
+goto cleanup
+
+@endlocal
+
+:Usage
+echo:
+echo The correct usage is:
+echo:
+echo     %0 [action] [architecture]
+echo:
+echo where
+echo:
+echo [action]           is: compile  ^| build
+echo [architecture]     is: x86      ^| x64
+echo:
+echo For example:
+echo     %0 compile x86    - compile only x86 (for CI)
+echo     %0 build x64      - build x64 client
+echo:
+goto :eof
+
+:cleanup
+endlocal
+exit /B %ERRORLEVEL%
---
+++ src/main.cpp
@@ -0,0 +1,295 @@
+/***************************************************************************
+ * Copyright (C) 2015-2019 by Savoir-faire Linux                           *
+ * Author: Edric Ladent Milaret <edric.ladent-milaret@savoirfairelinux.com>*
+ * Author: Andreas Traczyk <andreas.traczyk@savoirfairelinux.com>          *
+ *                                                                         *
+ * This program is free software; you can redistribute it and/or modify    *
+ * it under the terms of the GNU General Public License as published by    *
+ * the Free Software Foundation; either version 3 of the License, or       *
+ * (at your option) any later version.                                     *
+ *                                                                         *
+ * This program is distributed in the hope that it will be useful,         *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ * GNU General Public License for more details.                            *
+ *                                                                         *
+ * You should have received a copy of the GNU General Public License       *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
+ **************************************************************************/
+
+#include "mainwindow/mainwindow.h"
+
+#include "dialogs/accountmigrationdialog.h"
+#include "globalinstances.h"
+#include "misc/networkmanager.h"
+#include "bridging/lrcinstance.h"
+#include "misc/pixbufmanipulator.h"
+#include "misc/runguard.h"
+#include "misc/utils.h"
+#include "misc/splashscreen.h"
+
+#include <QApplication>
+#include <QFile>
+#include <QMessageBox>
+#include <QFontDatabase>
+#include <QLibraryInfo>
+#include <QTranslator>
+
+#include <ciso646>
+#include <locale.h>
+
+#ifdef Q_OS_WIN
+#include <windows.h>
+#endif
+
+#if defined _MSC_VER && !COMPILE_ONLY
+#include <gnutls/gnutls.h>
+#endif
+
+void
+consoleDebug()
+{
+#ifdef Q_OS_WIN
+    AllocConsole();
+    SetConsoleCP(CP_UTF8);
+
+    freopen("CONOUT$", "w", stdout);
+    freopen("CONOUT$", "w", stderr);
+
+    COORD coordInfo;
+    coordInfo.X = 130;
+    coordInfo.Y = 9000;
+
+    SetConsoleScreenBufferSize(GetStdHandle(STD_OUTPUT_HANDLE), coordInfo);
+    SetConsoleMode(GetStdHandle(STD_OUTPUT_HANDLE), ENABLE_QUICK_EDIT_MODE | ENABLE_EXTENDED_FLAGS);
+#endif
+}
+
+#ifdef _MSC_VER
+void
+vsConsoleDebug()
+{
+    // Print debug to output window if using VS
+    QObject::connect(
+        &LRCInstance::behaviorController(),
+        &lrc::api::BehaviorController::debugMessageReceived,
+        [](const std::string& message) {
+            OutputDebugStringA((message + "\n").c_str());
+        });
+}
+#endif
+
+void
+fileDebug(QFile& debugFile)
+{
+    QObject::connect(
+        &LRCInstance::behaviorController(),
+        &lrc::api::BehaviorController::debugMessageReceived,
+        [&debugFile](const std::string& message) {
+            if (debugFile.open(QIODevice::WriteOnly | QIODevice::Append)) {
+                auto msg = (message + "\n").c_str();
+                debugFile.write(msg, qstrlen(msg));
+                debugFile.close();
+            }
+        });
+}
+
+void
+exitApp(RunGuard& guard)
+{
+    GlobalSystemTray::instance().hide();
+    guard.release();
+}
+
+int
+main(int argc, char* argv[])
+{
+    setlocale(LC_ALL, "en_US.utf8");
+
+    char ARG_DISABLE_WEB_SECURITY[] = "--disable-web-security";
+    int newArgc = argc + 1 + 1;
+    char** newArgv = new char*[newArgc];
+    for (int i = 0; i < argc; i++) {
+        newArgv[i] = argv[i];
+    }
+    newArgv[argc] = ARG_DISABLE_WEB_SECURITY;
+    newArgv[argc + 1] = nullptr;
+
+#if defined(Q_OS_WIN) && (PROCESS_DPI_AWARE)
+    SetProcessDPIAware();
+#endif // Q_OS_WIN
+
+    QApplication a(newArgc, newArgv);
+
+    QCoreApplication::setApplicationName("Ring");
+    QCoreApplication::setOrganizationDomain("jami.net");
+
+    QCryptographicHash appData(QCryptographicHash::Sha256);
+    appData.addData(QApplication::applicationName().toUtf8());
+    appData.addData(QApplication::organizationDomain().toUtf8());
+    RunGuard guard(appData.result());
+    if (!guard.tryToRun()) {
+        return 0;
+    }
+
+    for (auto string : QCoreApplication::arguments()) {
+        if (string == "-d" || string == "--debug") {
+            consoleDebug();
+        }
+    }
+
+    Utils::removeOldVersions();
+
+    QApplication::setAttribute(Qt::AA_EnableHighDpiScaling, false);
+    QGuiApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
+
+    auto startMinimized = false;
+    QString uri = "";
+
+    auto appDir = qApp->applicationDirPath() + "/";
+    const auto locale_name = QLocale::system().name();
+    const auto locale_lang = locale_name.split('_')[0];
+
+    QTranslator qtTranslator_lang;
+    QTranslator qtTranslator_name;
+    if (locale_name != locale_lang) {
+        if (qtTranslator_lang.load("qt_" + locale_lang, QLibraryInfo::location(QLibraryInfo::TranslationsPath)))
+            a.installTranslator(&qtTranslator_lang);
+    }
+    qtTranslator_name.load("qt_" + locale_name, QLibraryInfo::location(QLibraryInfo::TranslationsPath));
+    a.installTranslator(&qtTranslator_name);
+
+    QTranslator lrcTranslator_lang;
+    QTranslator lrcTranslator_name;
+    if (locale_name != locale_lang) {
+        if (lrcTranslator_lang.load(appDir + "share/libringclient/translations/lrc_" + locale_lang))
+            a.installTranslator(&lrcTranslator_lang);
+    }
+    if (lrcTranslator_name.load(appDir + "share/libringclient/translations/lrc_" + locale_name))
+        a.installTranslator(&lrcTranslator_name);
+
+    QTranslator mainTranslator_lang;
+    QTranslator mainTranslator_name;
+    if (locale_name != locale_lang) {
+        if (mainTranslator_lang.load(appDir + "share/ring/translations/ring_client_windows_" + locale_lang))
+            a.installTranslator(&mainTranslator_lang);
+    }
+    if (mainTranslator_name.load(appDir + "share/ring/translations/ring_client_windows_" + locale_name))
+        a.installTranslator(&mainTranslator_name);
+
+    QFont font;
+    font.setFamily("Segoe UI");
+    a.setFont(font);
+
+#ifndef DEBUG_STYLESHEET
+    QFile file(":/stylesheet.css");
+    if (file.open(QIODevice::ReadOnly | QIODevice::Text)) {
+        a.setStyleSheet(file.readAll());
+        file.close();
+    }
+#endif
+
+#if defined _MSC_VER && !COMPILE_ONLY
+    gnutls_global_init();
+#endif
+
+    GlobalInstances::setPixmapManipulator(std::make_unique<PixbufManipulator>());
+
+    SplashScreen* splash = new SplashScreen();
+    std::atomic_bool isMigrating(false);
+    LRCInstance::init(
+        [&splash, &a, &isMigrating] {
+            splash->setupUI(
+                QPixmap(":/images/logo-jami-standard-coul.png"),
+                QString("Jami - ") + QObject::tr("Migration needed"),
+                QObject::tr("Migration in progress... This may take a while."),
+                QColor(232, 232, 232)
+            );
+            splash->show();
+            isMigrating = true;
+            while (isMigrating) {
+                a.processEvents();
+            }
+        },
+        [&splash, &isMigrating] {
+            while (!isMigrating) {
+                std::this_thread::sleep_for(std::chrono::milliseconds(10));
+            }
+            isMigrating = false;
+        });
+    splash->hide();
+    LRCInstance::subscribeToDebugReceived();
+
+    QDir logPath(QStandardPaths::writableLocation(
+        QStandardPaths::AppLocalDataLocation));
+    // since logPath will be .../Ring, we use cdUp to remove it.
+    logPath.cdUp();
+    QFile debugFile(logPath.absolutePath() + "/jami/jami.log");
+
+    for (auto string : QCoreApplication::arguments()) {
+        if (string.startsWith("jami:")) {
+            uri = string;
+        } else {
+            if (string == "-m" || string == "--minimized") {
+                startMinimized = true;
+            }
+            auto dbgFile = string == "-f" || string == "--file";
+            auto dbgConsole = string == "-c" || string == "--vsconsole";
+            if (dbgFile || dbgConsole) {
+                if (dbgFile) {
+                    debugFile.open(QIODevice::WriteOnly | QIODevice::Truncate);
+                    debugFile.close();
+                    fileDebug(debugFile);
+                }
+#ifdef _MSC_VER
+                if (dbgConsole) {
+                    vsConsoleDebug();
+                }
+#endif
+            }
+        }
+    }
+
+    auto accountList = LRCInstance::accountModel().getAccountList();
+
+    for (const std::string& i : accountList) {
+        auto accountStatus = LRCInstance::accountModel().getAccountInfo(i).status;
+        if (accountStatus == lrc::api::account::Status::ERROR_NEED_MIGRATION) {
+            std::unique_ptr<AccountMigrationDialog> dialog = std::make_unique<AccountMigrationDialog>(nullptr, i);
+            int status = dialog->exec();
+
+            //migration failed
+            if (!status) {
+#ifdef Q_OS_WIN
+                FreeConsole();
+#endif
+                exitApp(guard);
+                return status;
+            }
+        }
+    }
+
+    splash->finish(&MainWindow::instance());
+    splash->deleteLater();
+
+    QFontDatabase::addApplicationFont(":/images/FontAwesome.otf");
+
+    MainWindow::instance().createThumbBar();
+
+    if (not startMinimized)
+        MainWindow::instance().showWindow();
+    else {
+        MainWindow::instance().showMinimized();
+        MainWindow::instance().hide();
+    }
+
+    QObject::connect(&a, &QApplication::aboutToQuit, [&guard] { exitApp(guard); });
+
+    auto ret = a.exec();
+
+#ifdef Q_OS_WIN
+    FreeConsole();
+#endif
+
+    return ret;
+}
