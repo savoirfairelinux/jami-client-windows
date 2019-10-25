bf4a4d35dd1cd9eb60f4573c3594553091921100
Ming Rui Zhang <mingrui.zhang@savoirfairelinux.com>
2019-10-25 13:58:43 -0400

    scm: add windows service app to monitor jami running progresses
    Change-Id: Ia0f0148bd3616edd913a5eaced30ae1718126d79

---
+++ ring-client-windows-scm/servicebase.h
@@ -0,0 +1,184 @@
+/***************************************************************************
+ * Copyright (C) 2019 by Savoir-faire Linux                                *
+ * Author: Mingrui Zhang <mingrui.zhang@savoirfairelinux.com>              *
+ *                                                                         *
+ * This program is free software; you can redistribute it and/or modify    *
+ * it under the terms of the GNU General Public License as published by    *
+ * the Free Software Foundation; either version 3 of the License, or       *
+ * (at your option) any later version.                                     *
+ *                                                                         *
+ * This program is distributed in the hope that it will be useful,         *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ * GNU General Public License for more details.                            *
+ *                                                                         *
+ * You should have received a copy of the GNU General Public License       *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
+ **************************************************************************/
+
+#pragma once
+
+#include <iostream>
+#include <string>
+
+#include <windows.h>
+#include <winsvc.h>
+
+class ServiceBase
+{
+public:
+
+    /**
+     * Register the executable for a service with scm.
+     * After you call scmRegister(ServiceBase), the SCM issues a Start command,
+     * which results in a call to the onStart method in the service. This
+     * method blocks until the service has stopped.
+     *
+     * @param service - reference to ServiceBase or its derived classes
+     */
+    static BOOL scmRegister(ServiceBase &service);
+
+    /**
+     * Returns the last Win32 error, in wstring format.
+     * Returns an empty string if there is no error.
+     *
+     * @return error string
+     */
+    static PWSTR GetLastErrorAsPWSTR();
+
+    /**
+     * Service base constructor
+     *
+     * @param serviceName - service name
+     * @param canStop - whether the service can be stopped
+     * @param canShutdown - wwhether the service can be notified when system shutdown occurs.
+     * @param canPauseContinue - whether the service can be paused and continued
+     */
+    explicit ServiceBase(PWSTR serviceName,
+                         BOOL canStop = TRUE,
+                         BOOL canShutdown = TRUE,
+                         BOOL canPauseContinue = TRUE);
+
+    virtual ~ServiceBase();
+
+    /**
+     * Stop the service.
+     */
+    void serviceStop();
+
+protected:
+    /**
+     * Executes when a Start command is sent to the service by the SCM
+     * or when the operating system starts (for a service that starts automatically).
+     *
+     * @param argc - number of command line arguments
+     * @param argv - str of command line arguments
+     */
+    virtual void onStartCallback(DWORD argc, PWSTR *argv);
+
+    /**
+     * Executes when a Stop command is sent to the service by the SCM.
+     */
+    virtual void onStopCallback();
+
+    /**
+     * Executes when a Pause command is sent to the service by the SCM.
+     */
+    virtual void onPauseCallback();
+
+    /**
+     * Executes when a Continue command is sent to the service by the SCM.
+     */
+    virtual void onContinueCallback();
+
+    /**
+     * Executes when a system shuts down is sent to the service by the SCM.
+     */
+    virtual void onShutdownCallback();
+
+    /**
+     * Set current service status and report the status to the SCM.
+     *
+     * @param currentState - current service state
+     * @param win32ExitCode - exit code
+     * @param waitHint - estimated time for pending operation, in milliseconds
+     */
+    void setServiceStatus(DWORD currentState,
+                          DWORD win32ExitCode = NO_ERROR,
+                          DWORD waitHint = 0);
+
+    /**
+     * Send a message to the application event log
+     *
+     * @param message - message to be sent
+     * @param type - the type of event to be sent, it contains:
+     *
+     * EVENTLOG_SUCCESS
+     * EVENTLOG_AUDIT_FAILURE
+     * EVENTLOG_AUDIT_SUCCESS
+     * EVENTLOG_ERROR_TYPE
+     * EVENTLOG_INFORMATION_TYPE
+     * EVENTLOG_WARNING_TYPE
+     */
+    void writeEventLogEntry(PWSTR message, WORD type);
+
+    /**
+     * Get the process handle that is specificied by processName
+     *
+     * @param processName - name of the process (executable)
+     * @param hProcess - reference of the process handle
+     *
+     * https://docs.microsoft.com/en-us/windows/win32/toolhelp/taking-a-snapshot-and-viewing-processes
+     */
+    BOOL findProcessHandle(std::wstring& processName, HANDLE& hProcess);
+
+private:
+
+    /**
+     * Entry point for the service. It registers the handler function for the
+     * service and starts the service.
+     *
+     * @param argc - number of command line arguments
+     * @param argv - str of command line arguments
+     */
+    static void WINAPI serviceMain(DWORD argc, LPWSTR* argv);
+
+    /**
+     * The function is called by the SCM whenever a control code is sent to
+     * the service.
+     *
+     * @param ctrl - control code
+     */
+    static void WINAPI serviceCtrlHandler(DWORD ctrl);
+
+    /**
+     * Start the service.
+     *
+     * @param argc - number of command line arguments
+     * @param argv - str of command line arguments
+     */
+    void serviceStart(DWORD argc, PWSTR *argv);
+
+    // Pause the service.
+    void servicePause();
+
+    // Resume the service after being paused.
+    void serviceContinue();
+
+    // Execute when the system is shutting down.
+    void serviceShutdown();
+
+    // The name of the service
+    PWSTR serviceName_;
+
+    // The status of the service
+    SERVICE_STATUS serviceStatus_;
+
+    // The service status handle
+    SERVICE_STATUS_HANDLE statusHandle_;
+
+    // The singleton service instance as
+    // a pointer point to itself
+    static ServiceBase* currentService_;
+};
+
---
+++ ring-client-windows-scm/serviceinstall.h
@@ -0,0 +1,156 @@
+/***************************************************************************
+ * Copyright (C) 2019 by Savoir-faire Linux                                *
+ * Author: Mingrui Zhang <mingrui.zhang@savoirfairelinux.com>              *
+ *                                                                         *
+ * This program is free software; you can redistribute it and/or modify    *
+ * it under the terms of the GNU General Public License as published by    *
+ * the Free Software Foundation; either version 3 of the License, or       *
+ * (at your option) any later version.                                     *
+ *                                                                         *
+ * This program is distributed in the hope that it will be useful,         *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ * GNU General Public License for more details.                            *
+ *                                                                         *
+ * You should have received a copy of the GNU General Public License       *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
+ **************************************************************************/
+
+#pragma once
+
+#include "servicebase.h"
+
+#include <stdio.h>
+#include <windows.h>
+
+void cleanUp(SC_HANDLE& handle);
+
+/**
+ * Install the current application as a service to the local scm database.
+ *
+ * @param serviceName - service name
+ * @param displayName - display name of the service
+ * @param startType - the service start option.
+ *        This parameter can be one of  the following values:
+ *
+ *        SERVICE_AUTO_START,
+ *        SERVICE_BOOT_START,
+ *        SERVICE_DEMAND_START,
+ *        SERVICE_DISABLED,
+ *        SERVICE_SYSTEM_START.
+ *
+ * @param dependencies - a pointer to a double null-terminated array of null-
+ *                       separated names of services or load ordering groups that the system
+ *                       must start before this service
+ * @param account - the name of the account under which the service runs.
+ * @param password - password to the account
+ */
+void
+installService(PWSTR serviceName,
+    PWSTR displayName,
+    DWORD startType,
+    PWSTR dependencies,
+    PWSTR account,
+    PWSTR password,
+    LPCWSTR applicationPath)
+{
+    SC_HANDLE schSCManager = NULL;
+    SC_HANDLE schService = NULL;
+
+    // Open the local default scm database
+    schSCManager = OpenSCManager(NULL, NULL, SC_MANAGER_CONNECT | SC_MANAGER_CREATE_SERVICE);
+    if (!schSCManager) {
+        std::wcout << "OpenSCManager failed: " << ServiceBase::GetLastErrorAsPWSTR() << std::endl;
+        cleanUp(schSCManager);
+        return;
+    }
+
+    // Install the service into SCM by calling CreateService
+    schService = CreateService(
+        schSCManager,                // SCManager database
+        serviceName,                 // Name of service
+        displayName,                 // Name to display
+        SERVICE_ALL_ACCESS,          // Desired access
+        SERVICE_WIN32_OWN_PROCESS,   // Service type
+        startType,                   // Service start type
+        SERVICE_ERROR_NORMAL,        // Error control type
+        applicationPath,             // Service's binary
+        NULL,                        // No load ordering group
+        NULL,                        // No tag identifier
+        NULL,                        // Dependencies
+        NULL,                        // Service running account
+        NULL                         // Password of the account
+    );
+
+    if (schService == NULL) {
+        std::wcout << "CreateService failed: " << ServiceBase::GetLastErrorAsPWSTR() << std::endl;
+        cleanUp(schSCManager);
+        cleanUp(schService);
+        return;
+    }
+
+    wprintf(L"%s is installed.\n", serviceName);
+}
+
+void
+uninstallService(PWSTR serviceName)
+{
+    SC_HANDLE schSCManager = NULL;
+    SC_HANDLE schService = NULL;
+    SERVICE_STATUS ssSvcStatus = {};
+
+    // Open the local default service control manager database
+    schSCManager = OpenSCManager(NULL, NULL, SC_MANAGER_CONNECT);
+    if (schSCManager == NULL) {
+        std::wcout << "OpenSCManager failed: " << ServiceBase::GetLastErrorAsPWSTR() << std::endl;
+        cleanUp(schSCManager);
+        return;
+    }
+
+    // Open the service with delete, stop, and query status permissions
+    schService = OpenService(schSCManager, serviceName, SERVICE_STOP | SERVICE_QUERY_STATUS | DELETE);
+    if (schService == NULL) {
+        std::wcout << "OpenService failed: " << ServiceBase::GetLastErrorAsPWSTR() << std::endl;
+        cleanUp(schSCManager);
+        cleanUp(schService);
+        return;
+    }
+
+    // Try to stop the service
+    if (ControlService(schService, SERVICE_CONTROL_STOP, &ssSvcStatus)) {
+        wprintf(L"Stopping %s.", serviceName);
+        Sleep(1000);
+
+        while (QueryServiceStatus(schService, &ssSvcStatus)) {
+            if (ssSvcStatus.dwCurrentState == SERVICE_STOP_PENDING) {
+                wprintf(L".");
+                Sleep(1000);
+            } else break;
+        }
+
+        if (ssSvcStatus.dwCurrentState == SERVICE_STOPPED) {
+            wprintf(L"\n%s is stopped.\n", serviceName);
+        } else {
+            wprintf(L"\n%s failed to stop.\n", serviceName);
+        }
+    }
+
+    // Now remove the service by calling DeleteService.
+    if (!DeleteService(schService)) {
+        std::wcout << "DeleteService failed: " << ServiceBase::GetLastErrorAsPWSTR() << std::endl;
+        cleanUp(schSCManager);
+        cleanUp(schService);
+        return;
+    }
+
+    wprintf(L"%s is removed.\n", serviceName);
+}
+
+void
+cleanUp(SC_HANDLE& handle) {
+    // cleanup for allocated resources.
+    if (handle) {
+        CloseServiceHandle(handle);
+        handle = NULL;
+    }
+}
