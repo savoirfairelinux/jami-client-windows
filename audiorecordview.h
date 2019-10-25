e956b939b56424557c6cf3eff2928ab17b7a9e28
Yang Wang <yang.wang@savoirfairelinux.com>
2019-10-25 15:09:29 -0400

    recordwidget: implement native qt widget to record video and audio clip in chatview
    Change-Id: I446027695f0f56fc5f352c530f7d556a03be2b9d

---
+++ audiorecordview.h
@@ -0,0 +1,96 @@
+/**************************************************************************
+* Copyright (C) 2015-2019 by Savoir-faire Linux                           *
+* Author: Yang Wang <yang.wang@savoirfairelinux.com>                      *
+*                                                                         *
+* This program is free software; you can redistribute it and/or modify    *
+* it under the terms of the GNU General Public License as published by    *
+* the Free Software Foundation; either version 3 of the License, or       *
+* (at your option) any later version.                                     *
+*                                                                         *
+* This program is distributed in the hope that it will be useful,         *
+* but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+* GNU General Public License for more details.                            *
+*                                                                         *
+* You should have received a copy of the GNU General Public License       *
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
+**************************************************************************/
+
+#pragma once
+
+#include <QWidget>
+#include <QPushButton>
+#include <QtMultimedia/QMediaPlayer>
+
+namespace Ui {
+class AudioRecordView;
+}
+
+class AudioRecordView final : public QWidget
+{
+    Q_OBJECT
+
+    Q_PROPERTY(bool deleteFileWhenInterrupt READ getDeleteFileWhenInterrupt WRITE setDeleteFileWhenInterrupt DESIGNABLE true NOTIFY deleteFileWhenInterruptChanged);
+
+public:
+    enum class AudioRecorderStatus
+    {
+        aboutToRecord,
+        recording,
+        recorded,
+        playing
+    };
+
+    bool getDeleteFileWhenInterrupt();
+    void setDeleteFileWhenInterrupt(bool value);
+
+signals:
+    void deleteFileWhenInterruptChanged();
+
+public:
+    explicit AudioRecordView(QWidget *parent = nullptr);
+    ~AudioRecordView();
+
+private:
+    void switchToAboutToRecordPage();
+    void switchToRecordingPage();
+    void switchToRecordedPage();
+    void switchToPlayingPage();
+
+    void deleteAudioRecord();
+
+    void startAudioRecord();
+    void finishAudioRecord();
+    void interruptAudioRecord(bool willDeleteFile);
+    void playAudio();
+
+
+private slots:
+    void on_audioPlayFinish(QMediaPlayer::MediaStatus status);
+    void on_audioPlayProgressUpdate(qint64 position);
+    void on_audioRecordProgressUpdate();
+
+    void on_playBtn_toggled(bool checked);
+    void on_startAndFinishRecordBtn_toggled(bool checked);
+
+    void on_sendBtn_pressed();
+    void on_reRecordBtn_pressed();
+    void on_stopAndDeleteBtn_pressed();
+    void on_stopPlayingBtn_pressed();
+
+
+private:
+    void setUpStatus(AudioRecorderStatus status);
+    void initializeOverlayBtnCheckState(QPushButton* btn, bool isToCheck);
+    QString convertTimeDisplayFromMilisecond(qint64 milisecond);
+
+private:
+    bool deleteFileWhenInterrupt_ = true;
+    QString recordedAudioFilePath_ = "";
+    AudioRecorderStatus status_;
+    int currentTime_ = 0;
+    QTimer* recordTimer;
+
+    QMediaPlayer* audioPlayer_;
+    Ui::AudioRecordView* ui;
+};
