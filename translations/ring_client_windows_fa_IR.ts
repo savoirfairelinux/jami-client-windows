fcfe56119450658f7434b6b7d019c5c3d69b5b46
Andreas Traczyk <andreas.traczyk@savoirfairelinux.com>
2019-11-14 16:52:22 -0500

    misc: bump release name to "Free as in Freedom"
    Change-Id: I9944eac347386e2f6343ade078cc9af8658c0f9a

--- translations/ring_client_windows_fa_IR.ts
+++ translations/ring_client_windows_fa_IR.ts
@@ -24,7 +24,7 @@
     </message>
     <message>
         <location filename="../aboutdialog.ui" line="547"/>
-        <source>Live Free or Die</source>
+        <source>Free as in Freedom</source>
         <translation>آزاد زندگی کنید یا بمیرید</translation>
     </message>
     <message>
--- translations/ring_client_windows_hu.ts
+++ translations/ring_client_windows_hu.ts
@@ -24,7 +24,7 @@
     </message>
     <message>
         <location filename="../aboutdialog.ui" line="547"/>
-        <source>Live Free or Die</source>
+        <source>Free as in Freedom</source>
         <translation>Élj szabadon vagy halj meg</translation>
     </message>
     <message>
