6f1df4d35e6fe771f9a1f6de7a46b341efa9649c
Yang Wang <yang.wang@savoirfairelinux.com>
2019-12-30 10:11:31 -0500

    videowidget: reimplement video widget base on d3d11 base
    Change-Id: I334a25448ad6871130c1658d2d7cb3aea8dfcec9

---
+++ shaders/ShaderJami.hlsl
@@ -0,0 +1,26 @@
+struct VS_Input {
+    float3 VertexCoor : POSITION_Vertex;
+    float3 TextureCoor: Texcoordinate;
+    float4 VertexColor: ColorVertex;
+};
+
+struct VS_Output {
+    float3 positionOut : SV_POSITION;
+    
+};
+
+VS_Output VS_main(VS_Input input){
+    VS_Output out_vtx;
+
+    return out_vtx;
+}
+
+struct PS_Output {
+    float4 ColorOutput : SV_Target;
+};
+
+PS_Output PS_main(VS_Output input) {
+    PS_Output out_px;
+
+    return out_px;
+}
