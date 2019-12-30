cc9f9c210432faf1d883051eb369061b7d51f2fe
Yang Wang <yang.wang@savoirfairelinux.com>
2019-12-30 10:11:31 -0500

    videowidget: reimplement video widget base on d3d11 base
    Change-Id: I334a25448ad6871130c1658d2d7cb3aea8dfcec9

--- src/videowidgetbase.cpp
+++ src/videowidgetbase.cpp
@@ -54,3 +54,111 @@ VideoWidgetBase::showEvent(QShowEvent* e)
     Q_UNUSED(e);
     emit visibilityChanged(true);
 }
+
+D3DVideoWidgetBase::D3DVideoWidgetBase(QColor bgColor, QWidget* parent)
+    : QWidget(parent)
+{
+    setAttribute(Qt::WA_PaintOnScreen, true);
+    setAttribute(Qt::WA_NativeWindow, true);
+
+    initialize();
+}
+
+D3DVideoWidgetBase::~D3DVideoWidgetBase()
+{
+    CleanUp();
+}
+
+void
+D3DVideoWidgetBase::forceRepaint()
+{
+    auto parent = qobject_cast<QWidget*>(this->parent());
+    if (parent) parent->setUpdatesEnabled(false);
+    repaint();
+    if (parent) parent->setUpdatesEnabled(true);
+}
+
+void
+D3DVideoWidgetBase::initialize()
+{
+    InitD3D();
+    InitScene();
+}
+
+void
+D3DVideoWidgetBase::hideEvent(QHideEvent* e)
+{
+    Q_UNUSED(e);
+    emit visibilityChanged(false);
+}
+
+void
+D3DVideoWidgetBase::showEvent(QShowEvent* e)
+{
+    Q_UNUSED(e);
+    emit visibilityChanged(true);
+}
+
+void
+D3DVideoWidgetBase::resizeEvent(QResizeEvent* event)
+{
+    ResizeD3D();
+}
+
+void
+D3DVideoWidgetBase::paintEvent(QPaintEvent* event)
+{
+}
+
+void
+D3DVideoWidgetBase::InitD3D()
+{
+}
+
+void
+D3DVideoWidgetBase::ResizeD3D()
+{
+    D3D11_OBJECT_RELEASE(renderTargetView_);
+
+    HRESULT hr;
+    swapChain_->ResizeBuffers(1, width(), height(), DXGI_FORMAT_R8G8B8A8_UNORM, 0);
+    //Create our BackBuffer
+    ID3D11Texture2D* backBuffer;
+    swapChain_->GetBuffer(0, __uuidof(ID3D11Texture2D), (void**)&backBuffer);
+
+    //Create our Render Target
+    hr = d3dDevice_->CreateRenderTargetView(backBuffer, NULL, &renderTargetView_);
+    D3D11_OBJECT_RELEASE(backBuffer);
+
+    //Set our Render Target
+    d3dDevContext_->OMSetRenderTargets(1, &renderTargetView_, NULL);
+
+    //Create the Viewport
+    D3D11_VIEWPORT viewport;
+    ZeroMemory(&viewport, sizeof(D3D11_VIEWPORT));
+
+    viewport.TopLeftX = 0;
+    viewport.TopLeftY = 0;
+    viewport.Width = width();
+    viewport.Height = height();
+    viewport.MinDepth = 0.0f;
+    viewport.MaxDepth = 1.0f;
+
+    //Set the Viewport
+    d3dDevContext_->RSSetViewports(1, &viewport);
+}
+
+void
+D3DVideoWidgetBase::InitScene()
+{
+}
+
+void
+D3DVideoWidgetBase::paintD3D()
+{
+}
+
+void
+D3DVideoWidgetBase::CleanUp()
+{
+}
