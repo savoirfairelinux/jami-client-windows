2ceb82a1dd14e83153ad5d3b3db003e15241320e
Yang Wang <yang.wang@savoirfairelinux.com>
2020-01-03 16:07:57 -0500

    video widget base: reimplement video widget based on d3d with qwindow
    - the hardware frame from cuda is ready for render now
    - could be prepared to refactor into qobject for further implementation
    
    Change-Id: I959fed487c4c781b45bff1f00d32420741594968

--- src-gpu/ShaderJami.hlsl
+++ src-gpu/ShaderJami.hlsl
@@ -8,19 +8,20 @@ struct VS_Output {
     float4 positionVSOut : SV_POSITION;
     float2 texCoorVSOut  : TEXCOR_VS2PX;
     float4 vertexColor   : VERTEX_COLOR_VS2PX;
+    bool isNV12 : USE_HW_D3D11;
 };
 
 cbuffer VS_ConstantBuffer {
     float2 aViewPortWidthAndHeight;
     float2 aWidthAndHeight;
     float angleToRotate;
-    bool isUsingD3D11HW;
+    bool isNV12;
 }
 
 VS_Output VS_main(VS_Input input){
     VS_Output out_vtx;
-    float3 postionAfterRotate = mul(float3x3(cos(radians(angleToRotate)),sin(radians(angleToRotate)), 0,
-                                   -sin(radians(angleToRotate)),cos(radians(angleToRotate)), 0,
+    float3 postionAfterRotate = mul(float3x3(cos(radians(angleToRotate)),-sin(radians(angleToRotate)), 0,
+                                   sin(radians(angleToRotate)),cos(radians(angleToRotate)), 0,
                                    0,                 0,                    1), input.VertexCoor);
     float2 imageTrueSizeVector;
     if((fmod(angleToRotate, 360.0) >= 45.0 && fmod(angleToRotate, 360.0) <= 135.0) ||
@@ -48,26 +49,50 @@ VS_Output VS_main(VS_Input input){
     out_vtx.positionVSOut = float4(postionAfterRotate ,1.0f);
     out_vtx.texCoorVSOut = input.TextureCoor;
     out_vtx.vertexColor = input.VertexColor;
+    out_vtx.isNV12 = isNV12;
 
     return out_vtx;
 }
 
+static const float3x3 YUVtoRGBCoeffMatrix =
+{
+	1.164383f,  1.164383f, 1.164383f,
+	0.000000f, -0.391762f, 2.017232f,
+	1.596027f, -0.812968f, 0.000000f
+};
+
+float3 ConvertYUVtoRGB(float3 yuv)
+{
+	yuv -= float3(0.062745f, 0.501960f, 0.501960f);
+	yuv = mul(yuv, YUVtoRGBCoeffMatrix);
+
+	return saturate(yuv);
+}
 
 SamplerState samplerState : register(s0);
 Texture2D texY : register(t0);
 Texture2D texU : register(t1);
 Texture2D texV : register(t2);
+Texture2D texUV_NV12 : register(t3);
 
 float4 PS_main(VS_Output input) : SV_Target  {
+    
     float3 yuv;
     float3 rgb;
-    yuv.x = texY.Sample(samplerState,input.texCoorVSOut).r;
-    yuv.y = texU.Sample(samplerState,input.texCoorVSOut).r - 0.5f;
-    yuv.z = texV.Sample(samplerState,input.texCoorVSOut).r - 0.5f;
+    if(input.isNV12) {
+        float2 texCoorUV = float2(input.texCoorVSOut.x / 2, input.texCoorVSOut.y);
+        
+        yuv.x = texY.Sample(samplerState,input.texCoorVSOut).r;
+        yuv.y = texUV_NV12.Sample(samplerState,texCoorUV).r;
+        yuv.z = texUV_NV12.Sample(samplerState,texCoorUV).g;
+    } else {
+        yuv.x = texY.Sample(samplerState,input.texCoorVSOut).r;
+        yuv.y = texU.Sample(samplerState,input.texCoorVSOut).r;
+        yuv.z = texV.Sample(samplerState,input.texCoorVSOut).r;
+    }
+    
+    rgb = ConvertYUVtoRGB(yuv);
 
-    rgb = mul(yuv, float3x3(1,1,1,
-               0,-0.39465,2.03211,
-               1.13983, -0.58060,  0));
     if(rgb.r < 0.1f && rgb.b < 0.1f && yuv.x < 0.1f) {
         rgb.g = 0.0f;
     }
