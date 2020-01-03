98b499d2af69c09b77f454f92ff8b6a03784763f
Yang Wang <yang.wang@savoirfairelinux.com>
2020-01-03 16:07:57 -0500

    video widget base: reimplement video widget based on d3d with qwindow
    Change-Id: I959fed487c4c781b45bff1f00d32420741594968

--- shaders/ShaderJami.hlsl
+++ shaders/ShaderJami.hlsl
@@ -8,6 +8,7 @@ struct VS_Output {
     float4 positionVSOut : SV_POSITION;
     float2 texCoorVSOut  : TEXCOR_VS2PX;
     float4 vertexColor   : VERTEX_COLOR_VS2PX;
+    bool useD3D11HWAccel;
 };
 
 cbuffer VS_ConstantBuffer {
@@ -48,6 +49,7 @@ VS_Output VS_main(VS_Input input){
     out_vtx.positionVSOut = float4(postionAfterRotate ,1.0f);
     out_vtx.texCoorVSOut = input.TextureCoor;
     out_vtx.vertexColor = input.VertexColor;
+    out_vtx.useD3D11HWAccel = isUsingD3D11HW;
 
     return out_vtx;
 }
@@ -57,17 +59,24 @@ SamplerState samplerState : register(s0);
 Texture2D texY : register(t0);
 Texture2D texU : register(t1);
 Texture2D texV : register(t2);
+Texture2D texD3DHW : register(t3);
 
 float4 PS_main(VS_Output input) : SV_Target  {
+    float3x3 convertMat = float3x3(1,1,1,
+               0,-0.39465,2.03211,
+               1.13983, -0.58060,  0);
+    
     float3 yuv;
     float3 rgb;
-    yuv.x = texY.Sample(samplerState,input.texCoorVSOut).r;
-    yuv.y = texU.Sample(samplerState,input.texCoorVSOut).r - 0.5f;
-    yuv.z = texV.Sample(samplerState,input.texCoorVSOut).r - 0.5f;
-
-    rgb = mul(yuv, float3x3(1,1,1,
-               0,-0.39465,2.03211,
-               1.13983, -0.58060,  0));
+    if(input.useD3D11HWAccel) {
+        yuv = texD3DHW.Sample(samplerState,input.texCoorVSOut);
+    } else {
+        yuv.x = texY.Sample(samplerState,input.texCoorVSOut).r;
+        yuv.y = texU.Sample(samplerState,input.texCoorVSOut).r - 0.5f;
+        yuv.z = texV.Sample(samplerState,input.texCoorVSOut).r - 0.5f;
+    }
+    
+    rgb = mul(yuv, convertMat);
     if(rgb.r < 0.1f && rgb.b < 0.1f && yuv.x < 0.1f) {
         rgb.g = 0.0f;
     }
