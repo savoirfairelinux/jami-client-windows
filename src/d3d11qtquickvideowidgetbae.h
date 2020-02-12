6607b19f75e456876e6e65c62e1fea0aa2388ed8
Yang Wang <yang.wang@savoirfairelinux.com>
2020-02-12 15:53:35 -0500

    d3d11qml: embed d3d11 renderer into qml-based client
    Change-Id: I71e1ca451396f46f5935dff565787868f0b0ad2e

---
+++ src/d3d11qtquickvideowidgetbae.h
@@ -0,0 +1,204 @@
+#pragma once
+
+#include <QObject>
+#include <QtQuick/QQuickItem>
+#include <QSGRendererInterface>
+#include <QtQuick/QQuickWindow>
+
+// D3D headers
+#ifndef interface struct
+#define interface struct
+#endif
+#include <wrl/client.h>
+#include <DirectXMath.h>
+
+#include <d3d11.h>
+#include <d3d10.h>
+#include <d3dcompiler.h>
+
+extern "C" {
+    // FFMPEG headers
+#include <libavutil/frame.h>
+#include <libavutil/display.h>
+#include <libavutil/hwcontext.h>
+#include <libavutil/hwcontext_cuda.h>
+
+// CUDA headers
+#include <cuda.h>
+#include <cudaD3D11.h>
+
+// Libmfx and Media SDK headers
+#include "mfxvideo.h"
+}
+#include "mfxvideo++.h"
+
+#define D3D11_OBJECT_RELEASE(p) {if(p) p->Release();}
+#define D3D11_OBJECT_RESET(p) {if(p) p.Reset();}
+#define ERROR_HANDLE_RUN(a,b){ if(FAILED((HRESULT)a)) {qDebug() << b;}}
+
+struct ConstantBufferDataStruct
+{
+    DirectX::XMFLOAT2 aViewPortWidthAndHeight;
+    DirectX::XMFLOAT2 aWidthAndHeight;
+    float angleToRotate = 0.0f;
+    bool isNV12 = false;
+};
+
+struct Vertex {
+    Vertex();
+    Vertex(float x, float y, float z,
+        float tx, float ty,
+        float cr, float cg, float cb, float ca)
+        : pos(x, y, z)
+        , texCoor(tx, ty)
+        , vertexColor(cr, cg, cb, ca) {};
+
+    DirectX::XMFLOAT3 pos;
+    DirectX::XMFLOAT2 texCoor;
+    DirectX::XMFLOAT4 vertexColor;
+};
+
+const std::vector<D3D11_INPUT_ELEMENT_DESC> input_layout_description = {
+    {"POSITION",0,DXGI_FORMAT_R32G32B32_FLOAT,0,0,D3D11_INPUT_PER_VERTEX_DATA,0},
+    {"Texcoordinate",0,DXGI_FORMAT_R32G32_FLOAT,0,12,D3D11_INPUT_PER_VERTEX_DATA,0},
+    {"COLOR",0,DXGI_FORMAT_R32G32B32A32_FLOAT,0,20,D3D11_INPUT_PER_VERTEX_DATA,0},
+};
+
+Vertex vertexArray[] =
+{
+    Vertex(-1.0f,1.0f,0.0f,0.0f,0.0f,1.0f,0.0f,0.0f,1.0f),
+    Vertex(1.0f, 1.0f,0.0f,1.0f,0.0f,0.0f,1.0f,0.0f,1.0f),
+    Vertex(1.0f,-1.0f,0.0f,1.0f,1.0f,0.0f,0.0f,1.0f,1.0f),
+    Vertex(-1.0f,-1.0f,0.0f,0.0f,1.0f,1.0f,1.0f,0.0f,1.0f),
+};
+
+DWORD indices[] = {
+    // front face
+    0,1,2,
+    2,3,0
+};
+
+class D3D11Renderer : public QObject {
+    Q_OBJECT
+public:
+    D3D11Renderer(QObject* parent = nullptr);
+    virtual ~D3D11Renderer();
+
+    void setViewportSize(const QRectF& rect) { m_viewportRect = rect; }
+    void setWindow(QQuickWindow* window) { m_window = window; }
+    void setParantingQuickItem(QQuickItem* parent) { m_parantingItem = parent; }
+
+    // use this function to update the frame textures when a new frame is inbound
+    virtual bool updateTextures(AVFrame* frame);
+    void cleanTextures();
+
+signals:
+    void frameStartSlotFinished();
+    void mainPassRecordingStartSlotFinished();
+
+protected:
+    void initializeShaderResourceViewWithTexture(Microsoft::WRL::ComPtr<ID3D11Texture2D> texture,
+                                                 Microsoft::WRL::ComPtr<ID3D11ShaderResourceView> shaderResourceView,
+                                                 int index);
+    void initializeTexture(Microsoft::WRL::ComPtr<ID3D11Texture2D> texture,
+                           Microsoft::WRL::ComPtr<ID3D11ShaderResourceView> shaderResourceView,
+                           bool isTextureToBeUpdatedOrRecreated,
+                           int width, int height, int index, DXGI_FORMAT textureFormat, D3D11_SUBRESOURCE_DATA resourceData);
+    void initializeTextureWithCUDA(Microsoft::WRL::ComPtr<ID3D11Texture2D> texture, Microsoft::WRL::ComPtr<ID3D11ShaderResourceView> shaderResourceView,
+                                   bool isTextureToBeUpdatedOrRecreated,int width, int height, int lineSize, int index, DXGI_FORMAT textureFormat,
+                                   CUdeviceptr startPtr, CUcontext cudaContext);
+    bool updateTextureFromCUDA(AVFrame* frame, bool isTextureToBeUpdatedOrRecreated);
+
+    void reInitializeLastFrameParameters();
+    void updateLastFrameParameters(AVFrame* frame);
+    bool areTexturesToBeUpdatedOrRecreated(AVFrame* frame);
+
+public slots:
+    // what to do when a frame starts
+    void frameStart();
+    // Drawing function call
+    void mainPassRecordingStart();
+
+private:
+    enum Stage {
+        VertexStage,
+        FragmentStage
+    };
+    void prepareShader(Stage stage);
+    QByteArray compileShader(Stage stage,
+        const QByteArray& source,
+        const QByteArray& entryPoint);
+    void init();
+
+    void cleanUp();
+
+    QRectF m_viewportRect;
+    QQuickWindow* m_window;
+    QQuickItem* m_parantingItem;
+
+    QByteArray m_vert;
+    QByteArray m_vertEntryPoint;
+    QByteArray m_frag;
+    QByteArray m_fragEntryPoint;
+
+    bool m_initialized = false;
+
+    Microsoft::WRL::ComPtr<ID3D11Device> d3dDevice_;
+    Microsoft::WRL::ComPtr<ID3D11DeviceContext> d3dDevContext_;
+    Microsoft::WRL::ComPtr<ID3D11RenderTargetView> renderTargetView_;
+
+    Microsoft::WRL::ComPtr<ID3D11Buffer> vertexBuffer_;
+    Microsoft::WRL::ComPtr<ID3D11Buffer> indiceBuffer_;
+
+    Microsoft::WRL::ComPtr<ID3D11VertexShader> vertexShader_;
+    Microsoft::WRL::ComPtr<ID3D11PixelShader> pixelShader_;
+
+    Microsoft::WRL::ComPtr<ID3D11SamplerState> sampler_;
+
+    Microsoft::WRL::ComPtr<ID3D11ShaderResourceView> shaderResourceViewY_;
+    Microsoft::WRL::ComPtr<ID3D11ShaderResourceView> shaderResourceViewU_;
+    Microsoft::WRL::ComPtr<ID3D11ShaderResourceView> shaderResourceViewV_;
+    Microsoft::WRL::ComPtr<ID3D11ShaderResourceView> shaderResourceViewNV12_;
+
+    Microsoft::WRL::ComPtr<ID3D11Texture2D> texY_;
+    Microsoft::WRL::ComPtr<ID3D11Texture2D> texU_;
+    Microsoft::WRL::ComPtr<ID3D11Texture2D> texV_;
+    Microsoft::WRL::ComPtr<ID3D11Texture2D> texUV_NV12_;
+
+    Microsoft::WRL::ComPtr<ID3D11InputLayout> inputLayout_;
+    Microsoft::WRL::ComPtr<ID3D11Buffer> constantBuffer_;
+    ConstantBufferDataStruct constantBufferDataStruct_;
+
+    struct {
+        AVPixelFormat LastFormat = AV_PIX_FMT_NONE;
+        int LastWidth = 0;
+        int LastHeight = 0;
+    } lastFrameParameters_;
+};
+
+class D3D11QtQuickVideoWidgetBase : public QQuickItem
+{
+    Q_OBJECT
+public:
+    D3D11QtQuickVideoWidgetBase();
+    virtual ~D3D11QtQuickVideoWidgetBase();
+
+public slots:
+    void sync();
+    void cleanupRenderer();
+
+protected slots:
+    void handleWindowChanged(QQuickWindow* win);
+
+protected:
+    void releaseResources() override;
+
+    D3D11Renderer* renderer_;
+};
+
+class D3D11QtQuickVideoPreviewWidget : public D3D11QtQuickVideoWidgetBase
+{
+public:
+    D3D11QtQuickVideoPreviewWidget();
+    virtual ~D3D11QtQuickVideoPreviewWidget();
+};
