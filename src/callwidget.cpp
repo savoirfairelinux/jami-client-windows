524ce22971febbf94c6c5840f5577cfc19eca009
Ming Rui Zhang <mingrui.zhang@savoirfairelinux.com>
2020-02-24 17:02:26 -0500

    callwidget: add splitter view and remove useless widgets
    Change-Id: I456aceffa3a04e52a5b7f8d36856378d4214df7a

--- src/callwidget.cpp
+++ src/callwidget.cpp
@@ -242,7 +242,7 @@ CallWidget::CallWidget(QWidget* parent) :
     // hide the call stack
     setCallPanelVisibility(false);
 
-    ui->containerWidget->setVisible(false);
+    setVisible(false);
     ui->sipCallerBestIdLabel->setVisible(false);
 }
 
@@ -255,7 +255,7 @@ CallWidget::~CallWidget()
 void
 CallWidget::navigated(bool to)
 {
-    ui->containerWidget->setVisible(to);
+    setVisible(to);
     if (to) {
         updateSmartList();
         connectConversationModel();
