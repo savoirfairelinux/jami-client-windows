30a2f682b38a064a360cf5a4662a0f00e781b053
Ming Rui Zhang <mingrui.zhang@savoirfairelinux.com>
2020-02-26 11:24:29 -0500

    mainapplication: initial commit for QGuiApplication intergration
    Change-Id: I0314a57c0df2663803b84a406ec3341643b628eb

--- src/main.cpp
+++ src/main.cpp
@@ -20,11 +20,8 @@
 
 #include "mainapplication.h"
 #include "runguard.h"
-#include "qmlclipboardadapter.h"
 
 #include <QCryptographicHash>
-#include <QQmlEngine>
-#include <QQmlApplicationEngine>
 
 int
 main(int argc, char* argv[])
@@ -35,8 +32,8 @@ main(int argc, char* argv[])
 
     // runguard to make sure that only one instance runs at a time
     QCryptographicHash appData(QCryptographicHash::Sha256);
-    appData.addData(QApplication::applicationName().toUtf8());
-    appData.addData(QApplication::organizationDomain().toUtf8());
+    appData.addData(QGuiApplication::applicationName().toUtf8());
+    appData.addData(QGuiApplication::organizationDomain().toUtf8());
     RunGuard guard(appData.result());
     if (!guard.tryToRun()) {
         // no need to exitApp since app is not set up
@@ -53,13 +50,6 @@ main(int argc, char* argv[])
         a.exitApp();
         return 0;
     }
-
-    // for deployment and register types
-    qmlRegisterType<QmlClipboardAdapter>("MyQClipboard", 1, 0, "QClipboard");
-
-    QQmlApplicationEngine engine;
-    engine.load(QUrl(QStringLiteral("qrc:/src/KeyBoardShortcutTable.qml")));
-    engine.load(QUrl(QStringLiteral("qrc:/src/UserProfileCard.qml")));
 
     // exec the application
     auto ret = a.exec();
