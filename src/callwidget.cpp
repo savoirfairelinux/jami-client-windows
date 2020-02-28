c6b62c5e5e0dfe8508f616e617cdd7bee9ae7c0a
Ming Rui Zhang <mingrui.zhang@savoirfairelinux.com>
2020-02-28 11:17:18 -0500

    callwidgets: ui high dpi adaptation
    Change-Id: If298a3b9bcd0fd94e9b532e6a88c9486e60e79b5

--- src/callwidget.cpp
+++ src/callwidget.cpp
@@ -290,12 +290,6 @@ CallWidget::navigated(bool to)
 void
 CallWidget::updateCustomUI()
 {
-    auto scalingRatio = Utils::getCurrentScalingRatio();
-    if (scalingRatio > 1.0) {
-        ui->messageView->setZoomFactor(1.15);
-    } else {
-        ui->messageView->setZoomFactor(1.0);
-    }
 }
 
 void
