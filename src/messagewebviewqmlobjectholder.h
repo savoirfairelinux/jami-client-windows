0e834273783349a4be69e959a58cb48ab8be33f2
Ming Rui Zhang <mingrui.zhang@savoirfairelinux.com>
2020-03-02 14:39:09 -0500

    mainview: callwidget implementation in qml
    Change-Id: Ie83374eba7751b688139bcd2aec0da12ad5c1469

---
+++ src/messagewebviewqmlobjectholder.h
@@ -0,0 +1,88 @@
+/***************************************************************************
+ * Copyright (C) 2020 by Savoir-faire Linux                                *
+ * Author: Mingrui Zhang   <mingrui.zhang@savoirfairelinux.com>            *
+ *                                                                         *
+ * This program is free software; you can redistribute it and/or modify    *
+ * it under the terms of the GNU General Public License as published by    *
+ * the Free Software Foundation; either version 3 of the License, or       *
+ * (at your option) any later version.                                     *
+ *                                                                         *
+ * This program is distributed in the hope that it will be useful,         *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ * GNU General Public License for more details.                            *
+ *                                                                         *
+ * You should have received a copy of the GNU General Public License       *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
+ **************************************************************************/
+
+#pragma once
+
+#include "lrcinstance.h"
+
+#include <QObject>
+#include <QString>
+
+// to ease the logic with the power of c++
+class MessageWebViewQmlObjectHolder : public QObject {
+    Q_OBJECT
+
+public:
+    explicit MessageWebViewQmlObjectHolder(QObject* parent = 0);
+    ~MessageWebViewQmlObjectHolder();
+
+    // Must call Q_INVOKABLE so that this function can be used in QML, qml to c++
+    Q_INVOKABLE void setMessageWebViewQmlObject(QObject* obj);
+    Q_INVOKABLE void setupChatView(const QString& uid);
+
+    // run corrsponding js functions, c++ to qml
+    void setMessagesVisibility(bool visible);
+    void requestSendMessageContent();
+    void setInvitation(bool show, const QString& contactUri = "", const QString& contactId = "");
+    void clear();
+    void printHistory(lrc::api::ConversationModel& conversationModel,
+                      const std::map<uint64_t, lrc::api::interaction::Info> interactions);
+    void setSenderImage(const QString& sender, const QString& senderImage);
+    void printNewInteraction(lrc::api::ConversationModel& conversationModel,
+                             uint64_t msgId,
+                             const lrc::api::interaction::Info& interaction);
+    void setMessagesImageContent(const QString& path, bool isBased64 = false);
+    void setMessagesFileContent(const QString& path);
+
+public slots:
+    void slotSendMessageContentSaved(const QString& content);
+    void slotMessagesCleared();
+    void slotMessagesLoaded();
+    void slotSendMessage(const QString& message);
+    void slotSetNewMessagesContent(const QString& path);
+
+private:
+    void setConversationProfileData(const lrc::api::conversation::Info& convInfo);
+    void newInteraction(const QString& accountId, const QString& convUid,
+                        uint64_t interactionId, const interaction::Info& interaction);
+
+    // Object pointer
+    QObject* messageWebViewQmlObject_;
+    
+    QString LastConvUid_;
+
+    QMetaObject::Connection imConnection_;
+    QMetaObject::Connection imVisibleConnection_;
+    QMetaObject::Connection callChangedConnection_;
+    QMetaObject::Connection imClickedConnection_;
+    QMetaObject::Connection crListSelectionConnection_;
+    QMetaObject::Connection modelSortedConnection_;
+    QMetaObject::Connection modelUpdatedConnection_;
+    QMetaObject::Connection filterChangedConnection_;
+    QMetaObject::Connection newConversationConnection_;
+    QMetaObject::Connection conversationRemovedConnection_;
+    QMetaObject::Connection newInteractionConnection_;
+    QMetaObject::Connection interactionStatusUpdatedConnection_;
+    QMetaObject::Connection conversationClearedConnection;
+    QMetaObject::Connection selectedCallChanged_;
+    QMetaObject::Connection smartlistSelectionConnection_;
+    QMetaObject::Connection interactionRemovedConnection_;
+    QMetaObject::Connection contactAddedConnection_;
+    QMetaObject::Connection callStatusChangedConnection_;
+    QMetaObject::Connection addedToConferenceConnection_;
+};
