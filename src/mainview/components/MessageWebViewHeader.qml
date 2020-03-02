4678ae57c17273eab19979523b5e22446de82cc3
Ming Rui Zhang <mingrui.zhang@savoirfairelinux.com>
2020-03-02 14:39:09 -0500

    mainview: callwidget implementation in qml
    Change-Id: Ie83374eba7751b688139bcd2aec0da12ad5c1469

---
+++ src/mainview/components/MessageWebViewHeader.qml
@@ -0,0 +1,162 @@
+import QtQuick 2.14
+import QtQuick.Window 2.14
+import QtQuick.Controls 2.14
+import QtQuick.Layouts 1.14
+import net.jami.constant.jamitheme 1.0
+
+import "../../commoncomponents"
+
+Rectangle {
+    id: messagingHeaderRect
+
+    property int buttonPreferredSize: 30
+    property string userAliasLabelText: ""
+    property string userUserNameLabelText: ""
+
+    signal backToWelcomeViewButtonClicked()
+
+    RowLayout {
+        id: messagingHeaderRectRowLayout
+
+        anchors.fill: parent
+
+        HoverableButton {
+            id: backToWelcomeViewButton
+
+            Layout.alignment: Qt.AlignVCenter | Qt.AlignLeft
+            Layout.leftMargin: 10
+            Layout.preferredWidth: buttonPreferredSize
+            Layout.preferredHeight: buttonPreferredSize
+
+            radius: 30
+            source: "qrc:/images/icons/ic_arrow_back_24px.svg"
+            backgroundColor: "white"
+            onExitColor: "white"
+
+            onClicked: {
+                messagingHeaderRect.backToWelcomeViewButtonClicked()
+            }
+        }
+
+        Rectangle {
+            id: userNameOrIdRect
+
+            Layout.alignment: Qt.AlignVCenter | Qt.AlignLeft
+            // width + margin
+            Layout.preferredWidth: Math.max(100, messagingHeaderRect.width - backToWelcomeViewButton.width - buttonGroup.width - 30 - 6 - 5)
+            Layout.preferredHeight: messagingHeaderRect.height
+            Layout.leftMargin: 5
+
+            ColumnLayout {
+                id: userNameOrIdColumnLayout
+
+                Label {
+                    id: userAliasLabel
+
+                    Layout.alignment: Qt.AlignVCenter
+                    Layout.preferredWidth: userNameOrIdRect.width
+                    Layout.preferredHeight: textMetricsuserAliasLabel.boundingRect.height
+                    Layout.topMargin: userNameOrIdRect.height / 2 - userAliasLabel.height - 4
+
+                    font.pointSize: textFontSize - 1
+
+                    horizontalAlignment: Text.AlignLeft
+                    verticalAlignment: Text.AlignVCenter
+
+                    text: textMetricsuserAliasLabel.elidedText
+                }
+
+                TextMetrics {
+                    id:     textMetricsuserAliasLabel
+                    font:   userAliasLabel.font
+                    //
+                    text:   userAliasLabelText
+                    elideWidth: userNameOrIdRect.width
+                    elide: Qt.ElideMiddle
+                }
+
+                Label {
+                    id: userUserNameLabel
+
+                    Layout.alignment: Qt.AlignVCenter
+                    Layout.preferredWidth: userNameOrIdRect.width
+                    Layout.preferredHeight: textMetricsuserUserNameLabel.boundingRect.height
+
+                    font.pointSize: textFontSize - 2
+                    color: JamiTheme.faddedFontColor
+
+                    horizontalAlignment: Text.AlignLeft
+                    verticalAlignment: Text.AlignVCenter
+
+                    text: textMetricsuserUserNameLabel.elidedText
+                }
+
+                TextMetrics {
+                    id:     textMetricsuserUserNameLabel
+                    font:   userUserNameLabel.font
+                    //
+                    text:   userUserNameLabelText
+                    elideWidth: userNameOrIdRect.width
+                    elide: Qt.ElideMiddle
+                }
+
+            }
+        }
+
+        Rectangle {
+            id: buttonGroup
+
+            Layout.alignment: Qt.AlignRight | Qt.AlignVCenter
+            Layout.preferredWidth: buttonPreferredSize * 3
+            Layout.preferredHeight: buttonPreferredSize
+            Layout.rightMargin: 20
+
+            HoverableButton {
+                id: startAAudioCallButton
+
+                anchors.left: buttonGroup.left
+                anchors.verticalCenter: buttonGroup.verticalCenter
+
+                height: buttonPreferredSize
+                width: buttonPreferredSize
+
+                radius: 30
+                source: "qrc:/images/icons/ic_phone_24px.svg"
+                backgroundColor: "white"
+                onExitColor: "white"
+            }
+
+            HoverableButton {
+                id: startAVideoCallButton
+
+                anchors.left: startAAudioCallButton.right
+                anchors.leftMargin: 3
+                anchors.verticalCenter: buttonGroup.verticalCenter
+
+                height: buttonPreferredSize
+                width: buttonPreferredSize
+
+                radius: 30
+                source: "qrc:/images/icons/ic_video_call_24px.svg"
+                backgroundColor: "white"
+                onExitColor: "white"
+            }
+
+            HoverableButton {
+                id: sendContactRequestButton
+
+                anchors.left: startAVideoCallButton.right
+                anchors.leftMargin: 3
+                anchors.verticalCenter: buttonGroup.verticalCenter
+
+                height: buttonPreferredSize
+                width: buttonPreferredSize
+
+                radius: 30
+                source: "qrc:/images/icons/ic_person_add_black_24dp_2x.png"
+                backgroundColor: "white"
+                onExitColor: "white"
+            }
+        }
+    }
+}
---
+++ src/messagewebviewqmlobjectholder.cpp
@@ -0,0 +1,148 @@
+/***************************************************************************
+ * Copyright (C) 2020 by Savoir-faire Linux                                *
+ * Author: Mingrui Zhang   <mingrui.zhang@savoirfairelinux.com>            *
+ *                                                                         *
+ * This program is free software; you can redistribute it and/or modify    *
+ * it under the terms of the GNU General Public License as published by    *
+ * the Free Software Foundation; either version 3 of the License, or       *
+ * (at your option) any later version.                                     *
+ *                                                                         *
+ * This program is distributed in the hope that it will be useful,         *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ * GNU General Public License for more details.                            *
+ *                                                                         *
+ * You should have received a copy of the GNU General Public License       *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
+ **************************************************************************/
+
+#include "messagewebviewqmlobjectholder.h"
+#include "webchathelpers.h"
+
+#include "utils.h"
+
+MessageWebViewQmlObjectHolder::MessageWebViewQmlObjectHolder(QObject* parent)
+    : QObject(parent)
+{
+
+}
+
+MessageWebViewQmlObjectHolder::~MessageWebViewQmlObjectHolder()
+{
+}
+
+void
+MessageWebViewQmlObjectHolder::setMessageWebViewQmlObject(QObject* obj)
+{
+    // Set the object pointer
+    messageWebViewQmlObject_ = obj;
+}
+
+void
+MessageWebViewQmlObjectHolder::setupChatView(const QString& uid)
+{
+    if (!currentConvUid_.isEmpty()) {
+        LastConvUid_ = currentConvUid_;
+    }
+    currentConvUid_ = uid;
+
+    auto& convInfo = LRCInstance::getConversationFromConvUid(uid);
+    if (convInfo.uid.isEmpty()) {
+        return;
+    }
+
+    QString contactURI = convInfo.participants.at(0);
+
+    bool isContact = false;
+    auto selectedAccountId = LRCInstance::getCurrAccId();
+    auto& accountInfo = LRCInstance::accountModel().getAccountInfo(selectedAccountId);
+    bool isRINGAccount = accountInfo.profileInfo.type == lrc::api::profile::Type::RING;
+
+    lrc::api::profile::Type contactType;
+    try {
+        auto contactInfo = accountInfo.contactModel->getContact(contactURI);
+        if (contactInfo.isTrusted) {
+            isContact = true;
+        }
+        contactType = contactInfo.profileInfo.type;
+    }
+    catch (...) {}
+
+    bool shouldShowSendContactRequestBtn = !isContact && contactType != lrc::api::profile::Type::SIP;
+    //ui->sendContactRequestButton->setVisible(shouldShowSendContactRequestBtn);
+
+    QMetaObject::invokeMethod(messageWebViewQmlObject_, "setMessagesVisibility", Q_ARG(QVariant, false));
+
+    //connect(LRCInstance::getCurrentConversationModel(), &ConversationModel::composingStatusChanged, ui->messageView, &MessageWebView::contactIsComposing);
+
+    Utils::oneShotConnect(messageWebViewQmlObject_, SIGNAL(toSendMessageContentSaved(QString)), this, SLOT(slotSendMessageContentSaved(QString)));
+
+    QMetaObject::invokeMethod(messageWebViewQmlObject_, "requestSendMessageContent");
+}
+
+void
+MessageWebViewQmlObjectHolder::slotSendMessageContentSaved(const QString& content)
+{
+    if (!LastConvUid_.isEmpty()) {
+        //LRCInstance::setContentDraft(LastConvUid_, LRCInstance::getCurrAccId(), content);
+    }
+
+    Utils::oneShotConnect(messageWebViewQmlObject_, SIGNAL(toMessagesCleared()), this, SLOT(slotMessagesCleared()));
+
+    QMetaObject::invokeMethod(messageWebViewQmlObject_, "setInvitation", Q_ARG(QVariant, false), Q_ARG(QVariant, ""), Q_ARG(QVariant, ""));
+    QMetaObject::invokeMethod(messageWebViewQmlObject_, "clear");
+    //auto restoredContent = LRCInstance::getContentDraft(convInfo.uid, accountId);
+    //ui->messageView->setSendMessageContent(restoredContent);
+    //ui->smartList->update();
+}
+
+void
+MessageWebViewQmlObjectHolder::slotMessagesCleared()
+{
+    auto& convInfo = LRCInstance::getConversationFromConvUid(currentConvUid_);
+    auto convModel = LRCInstance::getCurrentConversationModel();
+
+    auto interactionsStr = interactionsToJsonArrayObject(*convModel, convInfo.interactions).toUtf8();
+    QMetaObject::invokeMethod(messageWebViewQmlObject_, "printHistory", Q_ARG(QVariant, interactionsStr.constData()));
+
+    Utils::oneShotConnect(messageWebViewQmlObject_, SIGNAL(toMessagesLoaded()), this, SLOT(slotMessagesLoaded()));
+
+    setConversationProfileData(convInfo);
+}
+
+void
+MessageWebViewQmlObjectHolder::slotMessagesLoaded()
+{
+    QMetaObject::invokeMethod(messageWebViewQmlObject_, "setMessagesVisibility", Q_ARG(QVariant, true));
+}
+
+void
+MessageWebViewQmlObjectHolder::setConversationProfileData(const lrc::api::conversation::Info& convInfo)
+{
+    /*auto convModel = LRCInstance::getCurrentConversationModel();
+    auto accInfo = &LRCInstance::getCurrentAccountInfo();
+    auto contactUri = convInfo.participants.front();
+    if (contactUri.isEmpty()) {
+        return;
+    }
+    try {
+        auto& contact = accInfo->contactModel->getContact(contactUri);
+        auto bestName = Utils::bestNameForConversation(convInfo, *convModel);
+        ui->messageView->setInvitation(
+            (contact.profileInfo.type == lrc::api::profile::Type::PENDING),
+            bestName,
+            contactUri
+        );
+        if (!contact.profileInfo.avatar.isEmpty()) {
+            ui->messageView->setSenderImage(contactUri, contact.profileInfo.avatar);
+        }
+        else {
+            auto avatar = Utils::conversationPhoto(convInfo.uid, *accInfo);
+            QByteArray ba;
+            QBuffer bu(&ba);
+            avatar.save(&bu, "PNG");
+            ui->messageView->setSenderImage(contactUri, QString::fromLocal8Bit(ba.toBase64()));
+        }
+    }
+    catch (...) {}*/
+}
