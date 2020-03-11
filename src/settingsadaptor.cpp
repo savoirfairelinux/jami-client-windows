0f8ecfcd70f8c30b8ec730c4468b3ea352f7fe62
Yang Wang <yang.wang@savoirfairelinux.com>
2020-03-11 11:01:29 -0400

    settingsview: implement setting widget in qml
    Change-Id: I6700e2b5c0a01d5a512416ec53711e8c09150567

---
+++ src/settingsadaptor.cpp
@@ -0,0 +1,245 @@
+/*
+ * Copyright (C) 2020 by Savoir-faire Linux
+ * Author: Yang Wang   <yang.wang@savoirfairelinux.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "settingsadaptor.h"
+
+#include "api/newdevicemodel.h"
+
+SettingsAdaptor::SettingsAdaptor(QObject *parent)
+    : QObject(parent)
+{}
+
+QString
+SettingsAdaptor::getDir_Document()
+{
+    return QDir::toNativeSeparators(
+        QStandardPaths::writableLocation(QStandardPaths::DocumentsLocation));
+}
+
+QString
+SettingsAdaptor::getDir_Download()
+{
+    QString downloadPath = QDir::toNativeSeparators(
+        LRCInstance::dataTransferModel().downloadDirectory);
+    int pos = downloadPath.lastIndexOf(QChar('\\'));
+    if (pos == downloadPath.length() - 1) {
+        downloadPath.truncate(pos);
+    }
+    return downloadPath;
+}
+
+bool
+SettingsAdaptor::getSettingsValue_CloseOrMinimized()
+{
+    QSettings settings("jami.net", "Jami");
+    return settings.value(SettingsKey::closeOrMinimized).toBool();
+}
+
+bool
+SettingsAdaptor::getSettingsValue_EnableNotifications()
+{
+    QSettings settings("jami.net", "Jami");
+    return settings.value(SettingsKey::enableNotifications).toBool();
+}
+
+bool
+SettingsAdaptor::getSettingsValue_AutoUpdate()
+{
+    QSettings settings("jami.net", "Jami");
+    return settings.value(SettingsKey::autoUpdate).toBool();
+}
+
+void
+SettingsAdaptor::setClosedOrMin(bool state)
+{
+    QSettings settings("jami.net", "Jami");
+    settings.setValue(SettingsKey::closeOrMinimized, state);
+}
+
+void
+SettingsAdaptor::setNotifications(bool state)
+{
+    QSettings settings("jami.net", "Jami");
+    settings.setValue(SettingsKey::enableNotifications, state);
+}
+
+void
+SettingsAdaptor::setUpdateAutomatic(bool state)
+{
+#ifdef Q_OS_WIN
+    QSettings settings("jami.net", "Jami");
+    settings.setValue(SettingsKey::autoUpdate, state);
+#endif
+}
+
+void
+SettingsAdaptor::setRunOnStartUp(bool state)
+{
+    if (Utils::CheckStartupLink(L"Jami")) {
+        if (!state) {
+            Utils::DeleteStartupLink(L"Jami");
+        }
+    } else if (state) {
+        Utils::CreateStartupLink(L"Jami");
+    }
+}
+
+void
+SettingsAdaptor::setDownloadPath(QString dir)
+{
+    QSettings settings("jami.net", "Jami");
+    settings.setValue(SettingsKey::downloadPath, dir);
+    LRCInstance::dataTransferModel().downloadDirectory = dir + "/";
+}
+
+lrc::api::video::ResRateList
+SettingsAdaptor::get_ResRateList(lrc::api::video::Channel channel, QString device)
+{
+    auto deviceCapabilities = get_DeviceCapabilities(device);
+
+    return deviceCapabilities[channel];
+}
+
+int
+SettingsAdaptor::get_DeviceCapabilitiesSize(const QString &device)
+{
+    return get_DeviceCapabilities(device).size();
+}
+
+QVector<QString>
+SettingsAdaptor::getResolutions(const QString &device)
+{
+    QVector<QString> resolutions;
+
+    auto currentSettings = LRCInstance::avModel().getDeviceSettings(device);
+
+    auto currentChannel = currentSettings.channel.isEmpty() ? "default" : currentSettings.channel;
+    auto channelCaps = get_ResRateList(currentChannel, device);
+    for (auto [resolution, frameRateList] : channelCaps) {
+        for (auto rate : frameRateList) {
+            resolutions.append(resolution);
+        }
+    }
+
+    return resolutions;
+}
+
+QVector<int>
+SettingsAdaptor::getFrameRates(const QString &device)
+{
+    QVector<int> rates;
+
+    auto currentSettings = LRCInstance::avModel().getDeviceSettings(device);
+
+    auto currentChannel = currentSettings.channel.isEmpty() ? "default" : currentSettings.channel;
+    auto channelCaps = get_ResRateList(currentChannel, device);
+    for (auto [resolution, frameRateList] : channelCaps) {
+        for (auto rate : frameRateList) {
+            rates.append((int) rate);
+        }
+    }
+
+    return rates;
+}
+
+lrc::api::video::Capabilities
+SettingsAdaptor::get_DeviceCapabilities(const QString &device)
+{
+    return LRCInstance::avModel().getDeviceCapabilities(device);
+}
+
+QString
+SettingsAdaptor::get_Video_Settings_Channel(const QString &deviceId)
+{
+    auto settings = LRCInstance::avModel().getDeviceSettings(deviceId);
+
+    return (QString) settings.channel;
+}
+
+QString
+SettingsAdaptor::get_Video_Settings_Name(const QString &deviceId)
+{
+    auto settings = LRCInstance::avModel().getDeviceSettings(deviceId);
+
+    return (QString) settings.name;
+}
+
+QString
+SettingsAdaptor::get_Video_Settings_Id(const QString &deviceId)
+{
+    auto settings = LRCInstance::avModel().getDeviceSettings(deviceId);
+
+    return (QString) settings.id;
+}
+
+qreal
+SettingsAdaptor::get_Video_Settings_Rate(const QString &deviceId)
+{
+    auto settings = LRCInstance::avModel().getDeviceSettings(deviceId);
+
+    return (qreal) settings.rate;
+}
+
+QString
+SettingsAdaptor::get_Video_Settings_Size(const QString &deviceId)
+{
+    auto settings = LRCInstance::avModel().getDeviceSettings(deviceId);
+
+    return (QString) settings.size;
+}
+
+void
+SettingsAdaptor::set_Video_Settings_Rate_And_Resolution(const QString &deviceId,
+                                                        qreal rate,
+                                                        const QString &resolution)
+{
+    auto settings = LRCInstance::avModel().getDeviceSettings(deviceId);
+    settings.rate = rate;
+    settings.size = resolution;
+    LRCInstance::avModel().setDeviceSettings(settings);
+}
+
+const lrc::api::account::Info &
+SettingsAdaptor::getCurrentAccountInfo()
+{
+    return LRCInstance::getCurrentAccountInfo();
+}
+
+const Q_INVOKABLE lrc::api::profile::Info &
+SettingsAdaptor::getCurrentAccount_Profile_Info()
+{
+    return LRCInstance::getCurrentAccountInfo().profileInfo;
+}
+
+ContactModel *
+SettingsAdaptor::getContactModel()
+{
+    return getCurrentAccountInfo().contactModel.get();
+}
+
+NewDeviceModel *
+SettingsAdaptor::getDeviceModel()
+{
+    return getCurrentAccountInfo().deviceModel.get();
+}
+
+int
+SettingsAdaptor::getCurrentAccount_Profile_Info_Type()
+{
+    return (int) (getCurrentAccount_Profile_Info().type);
+}
---
+++ src/settingsview/components/IconButton.qml
@@ -0,0 +1,113 @@
+import QtQuick 2.14
+import QtQuick.Window 2.14
+import QtQuick.Controls 2.14
+import QtQuick.Layouts 1.14
+import QtQuick.Controls.Universal 2.12
+import QtGraphicalEffects 1.14
+
+import "../../constant"
+
+Button {
+    id: button
+    checkable: true
+    hoverEnabled: true
+
+    property alias imageSource: buttonPix.source
+    property alias buttonText: buttonText.text
+
+    property string backgroundColor: JamiTheme.releaseColor
+    property string onPressColor: JamiTheme.pressColor
+    property string onReleaseColor: JamiTheme.releaseColor
+    property string onEnterColor: JamiTheme.hoverColor
+    property string onExitColor: JamiTheme.transparentColor
+    property string checkedColor: JamiTheme.releaseColor
+
+    signal checkedToggledForLeftPanel(var checked)
+    signal checkedToggledForRightPanel(var checked)
+
+    function setCheckedState(check, triggerSignal) {
+        button.checked = check
+        if (triggerSignal) {
+            checkedToggledForLeftPanel(check)
+            checkedToggledForRightPanel(check)
+        }
+        button.background.color = check ? button.checkedColor : button.onExitColor
+    }
+
+    onClicked: {
+        setCheckedState(true, true)
+    }
+
+    Layout.minimumHeight: 60
+    Layout.preferredHeight: 60
+    Layout.maximumHeight: 60
+
+    Layout.fillWidth: true
+
+    background: Rectangle {
+        anchors.fill: parent
+        color: parent.checked ? button.checkedColor : button.onExitColor
+
+        RowLayout {
+            anchors.fill: parent
+            spacing: 24
+            Image {
+                id: buttonPix
+                Layout.minimumHeight: 24
+                Layout.preferredHeight: 24
+                Layout.maximumHeight: 24
+
+                Layout.minimumWidth: 24
+                Layout.preferredWidth: 24
+                Layout.maximumWidth: 24
+
+                Layout.leftMargin: 24
+
+                Layout.alignment: Qt.AlignLeft | Qt.AlignVCenter
+            }
+
+            Label {
+                id: buttonText
+
+                Layout.alignment: Qt.AlignLeft | Qt.AlignVCenter
+
+                Layout.fillHeight: true
+                Layout.fillWidth: true
+
+                font.pointSize: 11
+                font.kerning: true
+
+                horizontalAlignment: Text.AlignLeft
+                verticalAlignment: Text.AlignVCenter
+            }
+        }
+
+        MouseArea {
+            anchors.fill: parent
+
+            hoverEnabled: true
+
+            onPressed: {
+                if (!button.checked) {
+                    parent.color = button.onPressColor
+                }
+            }
+            onReleased: {
+                button.clicked()
+                if (!button.checked) {
+                    parent.color = button.onExitColor
+                }
+            }
+            onEntered: {
+                if (!button.checked) {
+                    parent.color = button.onEnterColor
+                }
+            }
+            onExited: {
+                if (!button.checked) {
+                    parent.color = button.onExitColor
+                }
+            }
+        }
+    }
+}
