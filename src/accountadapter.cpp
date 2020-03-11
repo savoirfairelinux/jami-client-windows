bceba051055b15862c8642fa6d9558a0a595d677
Yang Wang <yang.wang@savoirfairelinux.com>
2020-03-11 11:01:29 -0400

    settingsview: implement setting widget in qml
    Change-Id: I6700e2b5c0a01d5a512416ec53711e8c09150567

--- src/accountadapter.cpp
+++ src/accountadapter.cpp
@@ -201,6 +201,36 @@ AccountAdapter::savePassword(const QString accountId,
     return LRCInstance::accountModel().changeAccountPassword(accountId, oldPassword, newPassword);
 }
 
+void
+AccountAdapter::startAudioMeter(bool async)
+{
+    LRCInstance::startAudioMeter(async);
+}
+
+void
+AccountAdapter::stopAudioMeter(bool async)
+{
+    LRCInstance::stopAudioMeter(async);
+}
+
+bool
+AccountAdapter::isPreviewing()
+{
+    return LRCInstance::renderer()->isPreviewing();
+}
+
+RenderManager *
+AccountAdapter::getRenderManager()
+{
+    return LRCInstance::renderer();
+}
+
+void
+AccountAdapter::setCurrAccDisplayName(QString text)
+{
+    LRCInstance::setCurrAccDisplayName(text);
+}
+
 bool
 AccountAdapter::hasPassword()
 {
@@ -220,6 +250,18 @@ NewAccountModel *
 AccountAdapter::accoundModel()
 {
     return &(LRCInstance::accountModel());
+}
+
+AVModel *
+AccountAdapter::avModel()
+{
+    return &(LRCInstance::avModel());
+}
+
+DataTransferModel *
+AccountAdapter::dataTransferModel()
+{
+    return &(LRCInstance::dataTransferModel());
 }
 
 void
