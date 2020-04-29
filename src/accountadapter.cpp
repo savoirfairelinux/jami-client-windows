064f65f428979198a43450042d7eb551a684dfae
Ming Rui Zhang <mingrui.zhang@savoirfairelinux.com>
2020-04-29 14:19:54 -0400

    misc: add qml base adapter and corresponding changes
    Change-Id: Idd4cb1a623ce64ed4b55f33174e1832a71092730

--- src/accountadapter.cpp
+++ src/accountadapter.cpp
@@ -1,47 +1,45 @@
-/***************************************************************************
- * Copyright (C) 2020 by Savoir-faire Linux                                *
- * Author: Edric Ladent Milaret <edric.ladent-milaret@savoirfairelinux.com>*
- * Author: Anthony Léonard <anthony.leonard@savoirfairelinux.com>          *
- * Author: Olivier Soldano <olivier.soldano@savoirfairelinux.com>          *
- * Author: Andreas Traczyk <andreas.traczyk@savoirfairelinux.com>          *
- * Author: Isa Nanic <isa.nanic@savoirfairelinux.com>                      *
- * Author: Mingrui Zhang <mingrui.zhang@savoirfairelinux.com>              *
- *                                                                         *
- * This program is free software; you can redistribute it and/or modify    *
- * it under the terms of the GNU General Public License as published by    *
- * the Free Software Foundation; either version 3 of the License, or       *
- * (at your option) any later version.                                     *
- *                                                                         *
- * This program is distributed in the hope that it will be useful,         *
- * but WITHOUT ANY WARRANTY; without even the implied warranty of          *
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
- * GNU General Public License for more details.                            *
- *                                                                         *
- * You should have received a copy of the GNU General Public License       *
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
- **************************************************************************/
+/*
+ * Copyright (C) 2020 by Savoir-faire Linux
+ * Author: Edric Ladent Milaret <edric.ladent-milaret@savoirfairelinux.com>
+ * Author: Anthony Léonard <anthony.leonard@savoirfairelinux.com
+ * Author: Olivier Soldano <olivier.soldano@savoirfairelinux.com>
+ * Author: Andreas Traczyk <andreas.traczyk@savoirfairelinux.com>
+ * Author: Isa Nanic <isa.nanic@savoirfairelinux.com>
+ * Author: Mingrui Zhang   <mingrui.zhang@savoirfairelinux.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
-#include "accountcomboboxqmlobjectholder.h"
+#include "accountadapter.h"
 
+#include "lrcinstance.h"
 #include "utils.h"
 
-AccountComboBoxQmlObjectHolder::AccountComboBoxQmlObjectHolder(QObject *parent)
-    : QObject(parent)
+AccountAdapter::AccountAdapter(QObject *parent)
+    : QmlAdapterBase(parent)
 {}
 
-AccountComboBoxQmlObjectHolder::~AccountComboBoxQmlObjectHolder() {}
+AccountAdapter::~AccountAdapter() {}
 
 void
-AccountComboBoxQmlObjectHolder::setAccountComboBoxQmlObject(QObject *obj)
+AccountAdapter::initQmlObject()
 {
-    // Set the object pointer
-    accountComboBoxQmlObject_ = obj;
-
     connectAccount(LRCInstance::getCurrAccId());
 }
 
 void
-AccountComboBoxQmlObjectHolder::accountChanged(int index)
+AccountAdapter::accountChanged(int index)
 {
     auto accountList = LRCInstance::accountModel().getAccountList();
     if (accountList.size() > index)
@@ -49,35 +47,29 @@ AccountComboBoxQmlObjectHolder::accountChanged(int index)
 }
 
 void
-AccountComboBoxQmlObjectHolder::setSelectedAccount(const QString &accountId, int index)
+AccountAdapter::setSelectedAccount(const QString &accountId, int index)
 {
     LRCInstance::setSelectedAccountId(accountId);
 
-    // First, we get back to the welcome view (except if in call)
-    // in call?
     backToWelcomePage(index);
 
     auto &accountInfo = LRCInstance::accountModel().getAccountInfo(accountId);
 
-    QMetaObject::invokeMethod(accountComboBoxQmlObject_,
-                              "updateSmartList",
-                              Q_ARG(QVariant, accountId));
+    QMetaObject::invokeMethod(qmlObj_, "updateSmartList", Q_ARG(QVariant, accountId));
     connectAccount(accountId);
     emit accountSignalsReconnect(accountId);
     //emit slotAccountChangedFinished();
 }
 
 void
-AccountComboBoxQmlObjectHolder::backToWelcomePage(int index)
+AccountAdapter::backToWelcomePage(int index)
 {
     deselectConversation();
-    QMetaObject::invokeMethod(accountComboBoxQmlObject_,
-                              "backToWelcomePage",
-                              Q_ARG(QVariant, index));
+    QMetaObject::invokeMethod(qmlObj_, "backToWelcomePage", Q_ARG(QVariant, index));
 }
 
 void
-AccountComboBoxQmlObjectHolder::deselectConversation()
+AccountAdapter::deselectConversation()
 {
     if (LRCInstance::getCurrentConvUid().isEmpty()) {
         return;
@@ -98,7 +90,7 @@ AccountComboBoxQmlObjectHolder::deselectConversation()
 }
 
 void
-AccountComboBoxQmlObjectHolder::connectAccount(const QString &accountId)
+AccountAdapter::connectAccount(const QString &accountId)
 {
     try {
         auto &accInfo = LRCInstance::accountModel().getAccountInfo(accountId);
