2db50f210e1083e1a8a9a038e2b3927e76129752
Yang Wang <yang.wang@savoirfairelinux.com>
2020-05-19 13:38:47 -0400

    wizard view: add photobooth to new wizard view in qml
    Change-Id: If5ec77f0ff42e728b6d167409594dd4453f66f36

--- src/utils.cpp
+++ src/utils.cpp
@@ -1020,6 +1020,12 @@ UtilsAdapter::stopPreviewing()
     }
 }
 
+Q_INVOKABLE bool
+UtilsAdapter::hasVideoCall()
+{
+    return LRCInstance::hasVideoCall();
+}
+
 const QString
 UtilsAdapter::getCallId(const QString &accountId, const QString &convUid)
 {
