b2625631b267ee1afda0fa27ed1b970901c26805
Ming Rui Zhang <mingrui.zhang@savoirfairelinux.com>
2020-05-29 11:15:24 -0400

    qml: initial commit for Jami qml client
    This initial commit contains most of the business logic of callwidget (old QWidget based client),
    which is now called mainview. There is currently no account creation page, settings page. To make
    sure that one can run it properly, there has to be at least one SIP or Jami account existing already.
    The missing logic will be implemented on top of this patch.
    
    Change-Id: Ifdf64816c80c2ba1e48a3f66912309ece0ee40d8

--- src/bannedlistmodel.h
+++ src/bannedlistmodel.h
@@ -1,20 +1,21 @@
-/***************************************************************************
-* Copyright (C) 2019-2019 by Savoir-faire Linux                                 *
-* Author: Isa Nanic <isa.nanic@savoirfairelinux.com>                       *
-*                                                                          *
-* This program is free software; you can redistribute it and/or modify     *
-* it under the terms of the GNU General Public License as published by     *
-* the Free Software Foundation; either version 3 of the License, or        *
-* (at your option) any later version.                                      *
-*                                                                          *
-* This program is distributed in the hope that it will be useful,          *
-* but WITHOUT ANY WARRANTY; without even the implied warranty of           *
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            *
-* GNU General Public License for more details.                             *
-*                                                                          *
-* You should have received a copy of the GNU General Public License        *
-* along with this program.  If not, see <http://www.gnu.org/licenses/>.    *
-***************************************************************************/
+/*
+ * Copyright (C) 2019-2020 by Savoir-faire Linux
+* Author: Isa Nanic <isa.nanic@savoirfairelinux.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #pragma once
 
 #include <QAbstractListModel>
@@ -22,12 +23,11 @@
 class BannedListModel : public QAbstractListModel
 {
     Q_OBJECT
-    BannedListModel(const BannedListModel& cpy);
+    BannedListModel(const BannedListModel &cpy);
 
 public:
     explicit BannedListModel(QObject *parent = nullptr);
 
     int rowCount(const QModelIndex &index = QModelIndex()) const override;
-    bool setData(const QModelIndex& index, const QVariant& value, int role = Qt::EditRole) override;
-
+    bool setData(const QModelIndex &index, const QVariant &value, int role = Qt::EditRole) override;
 };
